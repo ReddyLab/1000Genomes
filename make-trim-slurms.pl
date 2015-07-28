0f75f9503b900f3b07e2de74426712fb4e28df0c
Bill Majoros <martiandna@gmail.com>
2015-07-27 20:49:41 -0400

    update

--- make-trim-slurms.pl
+++ make-trim-slurms.pl
@@ -1,21 +1,35 @@
 #!/usr/bin/perl
 use strict;
+use SlurmWriter;
 
+my $NUM_JOBS=100;
 my $FASTQ_DIR="/data/common/1000_genomes/RNA/fastq";
-my $SLURM_DIR="";
-my $OUT_DIR="";
+my $ANALYSIS_DIR="/data/reddylab/Reference_Data/1000Genomes/analysis/trim";
+my $SLURM_DIR="$ANALYSIS_DIR/slurm";
+my $OUT_DIR="$ANALYSIS_DIR/output";
+my $UNPAIRED_DIR="$ANALYSIS_DIR/output/unpaired";
+my $ADAPTERS="/data/reddylab/software/Trimmomatic-0.33/Trimmomatic-0.33/adapters/TruSeq3-PE-2.fa";
 
+if(-e $SLURM_DIR) { system("rm -r $SLURM_DIR") }
+system("mkdir -p $SLURM_DIR");
+system("mkdir -p $OUT_DIR");
+system("mkdir -p $UNPAIRED_DIR");
+my $slurm=new SlurmWriter;
+$slurm->nice();
 my @fastq=`ls $FASTQ_DIR/*.fastq.gz`;
 foreach my $infile (@fastq) {
   next unless($infile=~/([^\/]+)_1.fastq.gz/);
   my $id=$1;
   my $infile1="$FASTQ_DIR/${id}_1.fastq.gz";
   my $infile2="$FASTQ_DIR/${id}_2.fastq.gz";
-  my $outfile1="";
-  my $outfile2="";
-  my $cmd="java -jar /data/reddylab/software/Trimmomatic-0.33/Trimmomatic-0.33/trimmomatic-0.33.jar PE -phred33 -threads 8 $infile1 $infile2 $outfile1 $unpaired1 $outfile2 $unpaired2 ILLUMINACLIP:/data/reddylab/software/Trimmomatic-0.33/Trimmomatic-0.33/adapters/TruSeq3-PE-2.fa:2:30:15:1:true HEADCROP:1 LEADING:30 SLIDINGWINDOW:7:20 MINLEN:30";
-
+  my $outfile1="$OUT_DIR/${id}_1.fastq.gz";
+  my $outfile2="$OUT_DIR/${id}_2.fastq.gz";
+  my $unpaired1="$UNPAIRED_DIR/${id}_1_unpaired.fastq.gz";
+  my $unpaired2="$UNPAIRED_DIR/${id}_2_unpaired.fastq.gz";
+  my $cmd="java -jar /data/reddylab/software/Trimmomatic-0.33/Trimmomatic-0.33/trimmomatic-0.33.jar PE -phred33 -threads 8 $infile1 $infile2 $outfile1 $unpaired1 $outfile2 $unpaired2 ILLUMINACLIP:$ADAPTERS:2:30:15:1:true HEADCROP:1 LEADING:30 SLIDINGWINDOW:7:20 MINLEN:30";
+  $slurm->addCommand($cmd);
 }
 
+$slurm->writeScripts($NUM_JOBS,$SLURM_DIR,"trim","lowmem",$ANALYSIS_DIR);
 
 
