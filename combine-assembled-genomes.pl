88343645524bed3a764857f6c21f1f332b1cea51
Bill Majoros <martiandna@gmail.com>
2015-12-30 06:55:46 -0500

    update

--- combine-assembled-genomes.pl
+++ combine-assembled-genomes.pl
@@ -1,12 +1,24 @@
 #!/usr/bin/perl
-use strict
+use strict;
 
 my $BASEDIR="/data/reddylab/Reference_Data/1000Genomes/analysis/assembly/fasta";
+my $OUTDIR="combined";
 my @dirs=(0,1,2,3,4,5,6,7,8,9);
 
 my @files=`ls $BASEDIR/0/*-?.fasta`;
 foreach my $file (@files) {
   chomp $file;
-  print "$file\n";
+  $file=~/\/([^\/]+-[12].fasta)/ || die;
+  $file=$1;
+  my $cmd="cd $BASEDIR ; cat ";
+  foreach my $subdir (@dirs) {$cmd.="$subdir/$file "}
+  $cmd.="> $OUTDIR/$file";
+  System();
+}
+
+sub System {
+  my ($cmd)=@_;
+  print "$cmd\n\n";
+  #system($cmd);
 }
 
