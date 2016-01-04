917d48b046fca849d514327c32f5f624edaedb60
Bill Majoros <martiandna@gmail.com>
2016-01-04 14:46:47 -0500

    update

---
+++ make-index-slurms.pl
@@ -0,0 +1,32 @@
+#!/usr/bin/perl
+use strict;
+
+my $BASEDIR="/data/reddylab/Reference_Data/1000Genomes/analysis/assembly/combined";
+my $SLURMS="$BASEDIR/index-slurms";
+
+my @IDs=`ls $BASEDIR`;
+foreach my $ID (@IDs) {
+  my $dir="$BASEDIR/$ID";
+  my $slurm="$SLURMS/$ID.slurm";
+  open(OUT,">$slurm") || die $slurm;
+  "!/bin/bash
+#
+#SBATCH --get-user-env
+#SBATCH -J $ID
+#SBATCH -o index.output
+#SBATCH -e index.output
+#SBATCH -A $ID
+#SBATCH --mem 100000
+#SBATCH --cpus-per-task=32
+#
+cd $dir
+/data/reddylab/software/STAR_2.4.2a/STAR-STAR_2.4.2a/bin/Linux_x86_64/STAR \\
+   --runMode genomeGenerate \\
+   --genomeDir $dir \\
+   --genomeFastaFiles $dir/*.fasta \\
+   --runThreadN 32
+";
+  close(OUT);
+
+}
+
