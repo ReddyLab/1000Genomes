999b68bb27fd410940514450126ed8b61b88a195
Bill Majoros <martiandna@gmail.com>
2016-01-06 10:08:23 -0500

    update

---
+++ add-haplotype-to-defline.pl
@@ -0,0 +1,19 @@
+#!/usr/bin/perl
+use strict;
+use ProgramName;
+
+my $name=ProgramName::get();
+die "$name <in.fasta> <haplotype[1/2]> <out.fasta>\n" unless @ARGV==3;
+my ($infile,$hap,$outfile)=@ARGV;
+
+open(OUT,">$outfile") || die $outfile;
+open(IN,$infile) || die $infile;
+while(<IN>) {
+  if(/^>(\S+)(.*)/) {
+    print OUT ">$1\_$hap$2";
+  }
+  else { print OUT }
+}
+close(IN);
+close(OUT);
+
--- make-index-slurms.pl
+++ make-index-slurms.pl
@@ -2,22 +2,27 @@
 use strict;
 
 my $BASEDIR="/data/reddylab/Reference_Data/1000Genomes/analysis/assembly/combined";
+my $IDs="/data/reddylab/Reference_Data/1000Genomes/analysis/assembly/Geuvadis-keep.txt";
 my $SLURMS="$BASEDIR/index-slurms";
 
-my @IDs=`ls $BASEDIR`;
+my @IDs;
+open(IN,$IDs) || die $IDs;
+while(<IN>) { chomp; push @IDs,$_ }
+close(IN);
+
 foreach my $ID (@IDs) {
   my $dir="$BASEDIR/$ID";
   my $slurm="$SLURMS/$ID.slurm";
   open(OUT,">$slurm") || die $slurm;
-  "!/bin/bash
+  print OUT "#!/bin/bash
 #
 #SBATCH --get-user-env
 #SBATCH -J $ID
-#SBATCH -o index.output
-#SBATCH -e index.output
+#SBATCH -o $ID.output
+#SBATCH -e $ID.output
 #SBATCH -A $ID
 #SBATCH --mem 100000
-#SBATCH --cpus-per-task=32
+#SBATCH --cpus-per-task=1
 #
 cd $dir
 /data/reddylab/software/STAR_2.4.2a/STAR-STAR_2.4.2a/bin/Linux_x86_64/STAR \\
