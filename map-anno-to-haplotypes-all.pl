8f95359d7c9f61289a01ffe1e09e0ea733de9cd4
Bill Majoros <martiandna@gmail.com>
2016-01-08 14:28:55 -0500

    update

---
+++ map-anno-to-haplotypes-all.pl
@@ -0,0 +1,24 @@
+#!/usr/bin/perl
+use strict;
+
+my $BASEDIR="/home/bmajoros/1000G/assembly/combined";
+my $GFF="/home/bmajoros/1000G/assembly/local-genes.gff";
+my $REF="$BASEDIR/ref";
+my $SRC="/home/bmajoros/1000G/src";
+
+my @subdirs=`ls $BASEDIR`;
+foreach my $dir (@subdirs) {
+  next if $dir eq "ref";
+  System("$SRC/map-anno-to-haplotypes.pl $GFF $REF $BASEDIR/$dir");
+
+}
+
+
+sub System
+{
+  my ($cmd)=@_;
+  print "$cmd\n";
+  system($cmd);
+}
+
+
