549bce744fa8d540161d69888eb4edc2c19e6dae
Bill Majoros <martiandna@gmail.com>
2016-01-09 07:00:13 -0500

    update

--- combine-assembled-genomes.pl
+++ combine-assembled-genomes.pl
@@ -1,10 +1,30 @@
 #!/usr/bin/perl
 use strict;
+use GffTranscriptReader;
 
-my $BASEDIR="/data/reddylab/Reference_Data/1000Genomes/analysis/assembly/fasta";
-my $OUTDIR="/data/reddylab/Reference_Data/1000Genomes/analysis/assembly/combined";
+# Globals
+my $THOUSAND="/home/bmajoros/1000G";
+my $ASSEMBLY="$THOUSAND/assembly";
+my $BASEDIR="$ASSEMBLY/fasta";
+my $OUTDIR="$ASSEMBLY/combined";
+my $GFF="$ASSEMBLY/genes-all-10.gff";
 my @dirs=(0,1,2,3,4,5,6,7,8,9);
 
+# Get list of nonredundant genes
+my $reader=new GffTranscriptReader();
+my $transcripts=$reader->loadGFF($GFF); # ordered along chrom
+my $numTrans=@$transcripts;
+my ($prevTrans,%keep);
+for(my $i=0 ; $i<$numTrans ; ++$i) {
+  my $transcript=$transcripts->[$i];
+  my $chr=$transcript->getSubstrate();
+  if($prevTrans && $prevTrans->getSubstrate() eq $chr &&
+     $prevTrans->overlaps($transcript)) { next }
+  $keep{$transcript->getGeneId()}=1;
+  $prevTrans=$transcript;
+}
+
+# Combine output files into one FASTA per individual
 my @files=`ls $BASEDIR/0/*-1.fasta`;
 foreach my $file (@files) {
   chomp $file;
@@ -12,17 +32,18 @@ foreach my $file (@files) {
   my $ID=$1;
   System("cd $OUTDIR ; mkdir $ID");
   my $outdir="$OUTDIR/$ID";
-  my $cmd="cd $BASEDIR ; cat ";
-  foreach my $subdir (@dirs) {$cmd.="$subdir/$ID-1.fasta "}
-  $cmd.="> $outdir/1.fasta";
-  System($cmd);
-  my $cmd="cd $BASEDIR ; cat ";
-  foreach my $subdir (@dirs) {$cmd.="$subdir/$ID-2.fasta "}
-  $cmd.="> $outdir/2.fasta";
-  System($cmd);
-  if($ID ne "ref") {
-    System("cd $outdir ; /home/bmajoros/1000G/src/add-haplotype-to-defline.pl 1.fasta 1 tmp.fasta ; mv tmp.fasta 1.fasta");
-    System("cd $outdir ; /home/bmajoros/1000G/src/add-haplotype-to-defline.pl 2.fasta 2 tmp.fasta ; mv tmp.fasta 2.fasta");
+
+  # Process 1.fasta
+  open(OUT,">$outdir/1.fasta") || die;
+  foreach my $subdir (@dirs) 
+    { append("$BASEDIR/$subdir/$ID-1.fasta",\*OUT,$ID,1) }
+  close(OUT);
+
+  # Process 2.fasta
+  open(OUT,">$outdir/2.fasta") || die;
+  foreach my $subdir (@dirs) 
+    { append("$BASEDIR/$subdir/$ID-2.fasta",\*OUT,$ID,2) }
+  close(OUT);
   }
 }
 
@@ -30,5 +51,22 @@ sub System {
   my ($cmd)=@_;
   #print "$cmd\n\n";
   system($cmd);
+}
+
+sub append
+{
+  my ($filename,$out,$ID,$haplotype)=@_;
+  open(IN,$filename) || die $filename;
+  while(<IN>) {
+    chomp;
+    if(/^>(\S+)(.*)/) {
+      my ($gene,$rest)=($1,$2);
+      next unless $keep{$gene};
+      if($ID ne "ref") {$gene.="_$haplotype"}
+      print $out ">$gene$rest\n";
+    }
+    else { print $out "$_\n" }
+  }
+  close(IN);
 }
 
---
+++ make-STAR-slurms.pl
@@ -0,0 +1,56 @@
+#!/usr/bin/perl
+use strict;
+
+my $THOUSAND="/home/bmajoros/1000G";
+my $BASEDIR="$THOUSAND/assembly/combined";
+my $IDs="$THOUSAND/assembly/Geuvadis-keep.txt";
+my $RNA_LIST="$THOUSAND/assembly/id-map-parsed.txt";
+my $SLURMS="$BASEDIR/STAR-slurms";
+my $RNA_DIR="$THOUSAND/trim/output";
+
+my @IDs;
+open(IN,$IDs) || die $IDs;
+while(<IN>) { chomp; push @IDs,$_ }
+close(IN);
+
+my %rnaHash;
+open(IN,$RNA_LIST) || die $RNA_LIST;
+while(<IN>) {
+  chomp;
+  my @fields=split; next unless @fields>=2;
+  my ($rnaID,$indivID)=@fields;
+  $rnaHash{$indivID}=$rnaID;
+}
+close(IN);
+
+foreach my $ID (@IDs) {
+  my $rnaID=$rnaHash{$ID}; die $ID unless $rnaID;
+  my $dir="$BASEDIR/$ID";
+  my $slurm="$SLURMS/$ID.slurm";
+  open(OUT,">$slurm") || die $slurm;
+  print OUT "#!/bin/bash
+#
+#SBATCH --get-user-env
+#SBATCH -J $ID
+#SBATCH -o $ID.output
+#SBATCH -e $ID.output
+#SBATCH -A $ID
+#SBATCH --mem 100000
+#SBATCH --cpus-per-task=1
+#
+cd $dir
+/data/reddylab/software/STAR_2.4.2a/STAR-STAR_2.4.2a/bin/Linux_x86_64/STAR \\
+  --genomeLoad LoadAndKeep \\
+  --genomeDir $dir \\
+  --readFilesIn $RNA_DIR/$rnaID\_1.fastq.gz $RNA_DIR/$rnaID\_2.fastq.gz \\
+  --readFilesCommand zcat \\
+  --outFileNamePrefix $ID \\
+  --outSAMstrandField intronMotif \\
+  --sjdbOverhang 74 \\
+  --runThreadN 1
+
+";
+  close(OUT);
+
+}
+
