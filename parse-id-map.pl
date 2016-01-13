52b65af2f0d05ab5cb5d7b63121371cbdfc8cf43
Bill Majoros <martiandna@gmail.com>
2016-01-13 07:42:37 -0500

    update

--- parse-id-map.pl
+++ parse-id-map.pl
@@ -3,6 +3,15 @@ use strict;
 
 my $THOUSAND="/home/bmajoros/1000G";
 my $RAW="$THOUSAND/assembly/id-map.txt";
+my $FASTQ="$THOUSAND/trim/output";
+
+my %keep;
+my @files=`ls $FASTQ`;
+foreach my $file (@files) {
+  chomp $file;
+  $file=~/(.*)_[12].fastq/ || next;
+  $keep{$1}=1;
+}
 
 my %hash;
 open(IN,$RAW) || die;
@@ -13,9 +22,11 @@ while(<IN>) {
   next unless @fields>=33;
   my $hg=$fields[0];
   my $err=$fields[28];
+  next unless $keep{$err};
   if(defined($hash{$hg})) {
     my $old=$hash{$hg};
     die "$err vs. $old\n" unless $old eq $err;
+    next;
   }
   print "$err\t$hg\n";
   $hash{$hg}=$err;
