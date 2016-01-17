d34f1bad1f8a61bfd0cfacac15e808c375aa317b
Bill Majoros <martiandna@gmail.com>
2016-01-17 09:33:51 -0500

    update

--- reformat-gff.pl
+++ reformat-gff.pl
@@ -1,18 +1,18 @@
 #!/usr/bin/perl
 use strict;
 
-my $THOUSAND="/home/bmajors/1000G";
+my $THOUSAND="/home/bmajoros/1000G";
 my $ASSEMBLY="$THOUSAND/assembly";
 my $COMBINED="$ASSEMBLY/combined";
 my $INFILE="mapped.gff";
 my $OUTFILE="reformatted.gff";
 
 my @dirs=`ls $COMBINED`;
-foreach my $dir (@$dirs) {
-  chomp $dir;
-  next unless $dir=~/^HG\d+$/ || $dir=~/^NA\d$/;
-  my $infile="$dir/$INFILE";
-  my $outfile="$dir/$OUTFILE";
+foreach my $subdir (@dirs) {
+  chomp $subdir;
+  next unless $subdir=~/^HG\d+$/ || $subdir=~/^NA\d$/;
+  my $infile="$COMBINED/$subdir/$INFILE";
+  my $outfile="$COMBINED/$subdir/$OUTFILE";
   process($infile,$outfile);
 }
 
@@ -38,7 +38,7 @@ sub process
     my ($left,$id,$right)=($1,$2,$3);
     $extra="${left}transcript_id \"${id}_$hap\"$right";
     if($type=~/exon/) { $type="exon" }
-    print OUT "$substrate\t$source\t$type\t$begin\t$end\t$score\t$strand\t$frame\t$extra";
+    print OUT "$substrate\t$source\t$type\t$begin\t$end\t$score\t$strand\t$frame\t$extra\n";
 
 #ENSG00000272636_1       ensembl internal-exon   17520   17599   0       -       2       transcript_id=ENST00000343572; gene_id=ENSG00000272636
   }
