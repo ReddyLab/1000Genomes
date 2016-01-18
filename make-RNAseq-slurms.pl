82009a5b1033341c82d6e3fde7097a44ea43cab2
Bill Majoros <martiandna@gmail.com>
2016-01-18 14:06:57 -0500

    update

--- make-RNAseq-slurms.pl
+++ make-RNAseq-slurms.pl
@@ -3,11 +3,11 @@ use strict;
 
 my $CPUs=32;
 my $THOUSAND="/home/bmajoros/1000G";
-my $BASEDIR="$THOUSAND/assembly/combined";
-my $IDs="$THOUSAND/assembly/Geuvadis-keep.txt";
+my $COMBINED="$THOUSAND/assembly/combined";
+#my $IDs="$THOUSAND/assembly/Geuvadis-keep.txt";
 my $RNA_LIST="$THOUSAND/assembly/id-map-parsed.txt";
 my $SLURMS="$THOUSAND/assembly/RNA-slurms";
-my $RNA_DIR="$THOUSAND/trim/output";
+my $FASTQ="$THOUSAND/trim/output";
 
 my @IDs;
 #open(IN,$IDs) || die $IDs;
@@ -17,6 +17,7 @@ my @dirs=`ls $COMBINED`;
 foreach my $subdir (@dirs) {
   chomp $subdir;
   next unless $subdir=~/^HG\d+$/ || $subdir=~/^NA\d+$/;
+  push @IDs,$subdir;
 }
 
 my %rnaHash;
@@ -37,34 +38,34 @@ foreach my $ID (@IDs) {
     print "not found: $ID\n";
     next;
   }
-  my $dir="$BASEDIR/$ID";
-  my $slurm="$SLURMS/$ID.slurm";
+  my $dir="$COMBINED/$ID";
+  my $slurm="$SLURMS/$slurmID.slurm";
   open(OUT,">$slurm") || die $slurm;
   print OUT "#!/bin/bash
 #
 #SBATCH --get-user-env
 #SBATCH -J RNA$slurmID
-#SBATCH -o $ID.output
-#SBATCH -e $ID.output
+#SBATCH -o $slurmID.output
+#SBATCH -e $slurmID.output
 #SBATCH -A RNA$slurmID
-#SBATCH --mem 40000
+#SBATCH --mem 10000
 #SBATCH --cpus-per-task=$CPUs
 #
 cd $dir
- $dir \\
-  $RNA_DIR/$rnaID\_1.fastq.gz $RNA_DIR/$rnaID\_2.fastq.gz \\
-  
-cat 1.fasta 2.fasta > 1and2.fa
 
-bowtie2-build 1and2.fa 1and2
+rm -rf RNA
 
 mkdir RNA
 
-tophat2 --output-dir /home/bmajoros/1000G/assembly/combined/HG00096/RNA --min-intron-length 30 --num-threads 32 --GTF /home/bmajoros/1000G/assembly/combined/HG00096/reformatted.gff 1and2 /home/bmajoros/1000G/trim/output/ERR188040_1.fastq.gz /home/bmajoros/1000G/trim/output/ERR188040_2.fastq.gz
+cd RNA
 
-/data/reddylab/software/stringtie/stringtie-1.2.1.Linux_x86_64/stringtie accepted_hits.bam -G /home/bmajoros/1000G/assembly/combined/HG00096/reformatted.gff -o stringtie.gff -p 1 -C stringtie.coverage -A stringtie.abundance
+cat ../1.fasta ../2.fasta > 1and2.fa
 
+bowtie2-build 1and2.fa 1and2
 
+tophat2 --output-dir $dir/RNA --min-intron-length 30 --num-threads $CPUs --GTF $dir/reformatted.gff 1and2 $FASTQ/$rnaID\_1.fastq.gz $FASTQ/$rnaID\_2.fastq.gz
+
+/data/reddylab/software/stringtie/stringtie-1.2.1.Linux_x86_64/stringtie accepted_hits.bam -G $dir/reformatted.gff -o stringtie.gff -p 1 -C stringtie.coverage -A stringtie.abundance
 ";
   close(OUT);
   ++$slurmID;
