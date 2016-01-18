2630bca7215a8ce51a394a04491f243204ffa303
Bill Majoros <martiandna@gmail.com>
2016-01-18 12:18:02 -0500

    update

--- summarize-splice-consensus.pl
+++ summarize-splice-consensus.pl
@@ -19,15 +19,35 @@ while(<IN>) {
 }
 close(IN);
 
-summarize(\%donors);
-summarize(\%acceptors);
+my $totalGT=summarize(\%donors,"GT","GC","AT");
+my $totalAG=summarize(\%acceptors,"AG","AC");
+print "DONORS $totalGT\n";
+print "ACCEPTORS $totalAG\n";
+dumpHash(\%donors,"donor");
+dumpHash(\%acceptors,"acceptor");
+
+sub dumpHash
+{
+  my ($hash,$label)=@_;
+  my @keys=keys %$hash;
+  foreach my $key (@keys) {
+    my $value=$hash->{$key};
+    print "$label\t$key\t$value\n";
+  }
+}
 
 sub summarize
 {
-  my ($hash)=@_;
+  my $hash=shift @_;
   my @keys=keys %$hash;
   foreach my $key (@keys) {
+    $hash->{$key}=$value/$n;
   }
+  my $total=0;
+  foreach my $seq (@_) {
+    $total+=$hash->{$seq};
+  }
+  return $total;
 }
 
 
