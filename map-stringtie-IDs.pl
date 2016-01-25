b6ae7f7e0d9b7012b7ff005db7df99cda00c8a94
Bill Majoros <martiandna@gmail.com>
2016-01-25 14:28:29 -0500

    update

---
+++ map-stringtie-IDs.pl
@@ -0,0 +1,40 @@
+#!/usr/bin/perl
+use strict;
+use GffTranscriptReader;
+use ProgramName;
+
+my $name=ProgramName::get();
+die "$name <in.gff> <out.gf>\n" unless @ARGV==2;
+my ($infile,$outfile)=@ARGV;
+
+my $reader=new GffTranscriptReader();
+my $transcripts=$reader->loadGFF($infile);
+open(OUT,">$outfile") || die $outfile;
+my $numTranscripts=@$transcripts;
+for(my $i=0 ; $i<$numTranscripts ; ++$i) {
+  my $transcript=$transcripts->[$i];
+  my $keyValuePairs=$transcript->parseExtraFields();
+  my $hash=$transcript->hashExtraFields($keyValuePairs);
+  my $substrate=$transcript->getSubstrate();
+  $transcript->setGeneId($substrate);
+  if(defined($hash->{"reference_id"})) {
+    my $transcriptID=$hash->{"reference_id"};
+    $transcript->setTranscriptId($transcriptId);
+  }
+  my @filteredPairs;
+  foreach my $pair (@$keyValuePairs) {
+    my ($key,$value)=@$pair;
+    next if $key eq "gene_id" || $key eq "transcript_id";
+    push @filteredPairs,$pair;
+  }
+  $transcript->setExtraFieldsFromKeyValuePairs(\@filteredPairs);
+  my $gff=$transcript->toGff();
+  print OUT $gff;
+}
+close(OUT);
+
+
+
+
+
+
