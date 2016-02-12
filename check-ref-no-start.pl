fb969235a5922317679c5734c6af2d64d7fc327e
Bill Majoros <martiandna@gmail.com>
2016-02-12 09:07:58 -0500

    update

--- check-ref-no-start.pl
+++ check-ref-no-start.pl
@@ -12,8 +12,7 @@ my $FBI_FILE="$GENOME_DIR/out.fbi";
 my $TWO_BIT="/data/reddylab/Reference_Data/hg19.2bit";
 my $ENSEMBL="/home/bmajoros/ensembl/coding-and-noncoding.gff";
 my $tempFile=TempFilename::generate();
-my %stopCodons;
-$stopCodons{"TGA"}=$stopCodons{"TAG"}=$stopCodons{"TAA"}=1;
+my %startCodons; $startCodons{"ATG"}=1;
 
 # Load the original ensembl GFF
 my $gffReader=new GffTranscriptReader();
@@ -29,7 +28,7 @@ while(1) {
 
   # Find a transcript with no-stop-codon
   next unless $code eq "bad-annotation";
-  my $array=$status->findDescendents("no-stop-codon");
+  my $array=$status->findDescendents("bad-start");
   next unless @$array>0;
   my $geneID=$report->getAttribute("substrate");
   my $transcriptID=$report->getAttribute("transcript-ID");
@@ -38,34 +37,34 @@ while(1) {
   # Get ensembl GFF for this transcript
   my $transcript=$byTranscriptID->{$transcriptID};
   die "$transcriptID not found in ensembl GFF" unless $transcript;
-  my $stopCoord=getStopCoord($transcript);
+  my $startCoord=getStopCoord($transcript);
   my $strand=$transcript->getStrand();
 
   # Extract sequence from 2bit file
   my $substrate=$transcript->getSubstrate();
-  if($strand eq "-") { $stopCoord-=3 }
-  my $begin=$stopCoord;
+  if($strand eq "+") { $startCoord-=3 }
+  my $begin=$startCoord;
   my $end=$begin+6;
   system("twoBitToFa -seq=$substrate -start=$begin -end=$end $TWO_BIT $tempFile");
   my $seq=FastaReader::firstSequence($tempFile); die unless length($seq)==6;
   if($strand eq "-") { $seq=Translation::reverseComplement(\$seq) }
   my $firstCodon=substr($seq,0,3); my $secondCodon=substr($seq,3,3);
-  if($stopCodons{$firstCodon} || $stopCodons{$secondCodon}) { next }
+  if($startCodons{$firstCodon} || $startCodons{$secondCodon}) { next }
   print "$firstCodon\t$transcriptID\n";
   ++$notFound;
 }
 unlink($tempFile) if -e $tempFile;
-print "$notFound of $sampleSize had no stop codon\n";
+print "$notFound of $sampleSize had no start codon\n";
 
 
 
 
-sub getStopCoord
+sub getStartCoord
 {
   my ($transcript)=@_;
   my $strand=$transcript->getStrand();
-  my $lastExon=$transcript->getIthExon($transcript->numExons()-1);
-  if($strand eq "+") { return $lastExon->getEnd()-3 }
-  else { return $lastExon->getBegin() }
+  my $firstExon=$transcript->getIthExon(0);
+  if($strand eq "+") { return $firstExon->getBegin() }
+  else { return $firstExon->getEnd()-3 }
 }
 
