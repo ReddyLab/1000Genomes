3a423950380f476235f479ad7ca1f5b5eac4f11e
Bill Majoros <martiandna@gmail.com>
2016-02-19 10:28:11 -0500

    update

---
+++ summarize-tabulated.pl
@@ -0,0 +1,17 @@
+#!/usr/bin/perl
+use strict;
+
+my $THOUSAND="/home/bmajoros/1000G";
+my $ASSEMBLY="$THOUSAND/assembly";
+my $COMBINED="$ASSEMBLY/combined";
+my $SLURM_DIR="$ASSEMBLY/tabulate-slurms";
+
+my @dirs=`ls $COMBINED`;
+my $slurmID=1;
+foreach my $subdir (@dirs) {
+  chomp $subdir;
+  next unless $subdir=~/^HG\d+$/ || $subdir=~/^NA\d+$/;
+  my $dir="$COMBINED/$subdir";
+
+
+}
