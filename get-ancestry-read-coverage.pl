b879d53da3304b5c48053903d94bfc7336f19992
Bill Majoros <martiandna@gmail.com>
2016-02-20 18:01:29 -0500

    update

--- get-ancestry-read-coverage.pl
+++ get-ancestry-read-coverage.pl
@@ -6,12 +6,12 @@ while(<IN>) {
   chomp;
   @fields=split; next unless @fields>=2;
   my ($id,$pop)=@fields;
-  next unless $pop eq "CEU";
+  #next unless $pop eq "CEU";
   open(ALIGN,"/home/bmajoros/1000G/assembly/combined/$id/RNA/align_summary.txt") || die;
-  while(<ALIGN>) {
-    
-  }
+  my $reads;
+  while(<ALIGN>) { if(/Input\s*:\s*(\d+)/) { $reads=$1; last } }
   close(ALIGN);
+  print "$pop\t$reads\n";
 }
 close(IN);
 
