273d1120e46bcef0701e7f631274b6bab46b55b4
Bill Majoros <martiandna@gmail.com>
2016-02-26 12:31:25 -0500

    update

--- tabulate-inactivated.pl
+++ tabulate-inactivated.pl
@@ -7,16 +7,17 @@ my $THOUSAND="/home/bmajoros/1000G";
 my $ASSEMBLY="$THOUSAND/assembly";
 my $COMBINED="$ASSEMBLY/combined";
 
+my %parent;
 my @dirs=`ls $COMBINED`;
 my $slurmID=1;
 print "transcript\tgene";
-#my @indiv;
+my @indiv;
 foreach my $subdir (@dirs) {
   chomp $subdir;
   next unless $subdir=~/^HG\d+$/ || $subdir=~/^NA\d+$/;
   my $dir="$COMBINED/$subdir";
   my $indiv=$subdir;
-#  push @indiv,$indiv;
+  push @indiv,$indiv;
   print "\t$indiv";
 }
 print "\n";
@@ -25,6 +26,9 @@ my %genes;
 foreach my $subdir (@dirs) {
   chomp $subdir;
   next unless $subdir=~/^HG\d+$/ || $subdir=~/^NA\d+$/;
+
+  next unless $subdir=~/HG00096/;
+
   my $dir="$COMBINED/$subdir";
   my $indiv=$subdir;
   my (%hash1,%hash2);
@@ -42,16 +46,21 @@ foreach my $subdir (@dirs) {
   my @keys=keys %combined;
   foreach my $key (@keys) {
     if($combined{$key}>$HOMOZYGOUS) {
-      push @{$genes{$key}},$indiv;
+      $genes{$key}->{$indiv}=1;
     }
   }
 }
 
-my @keys=keys %hash;
-foreach my $key (@keys) {
-  my $array=$hash{$key};
-
-
+my @transcripts=keys %genes;
+foreach my $transcript (@transcripts) {
+  my $parent=$parent{$transcript};
+  print "$transcript\t$parent";
+  my $hash=$genes{$transcript};
+  foreach my $indiv (@indiv) {
+    my $status=0+$hash->{$indiv};
+    print "\t$status";
+  }
+  print "\n":
 }
 
 
@@ -62,7 +71,8 @@ sub process
   while(<IN>) {
     chomp; my @fields=split; next unless @fields>3;
     my ($gene,$transcript,$event,$why)=@fields;
-    $hash->{$gene}=[$event,$why];
+    $hash->{$transcript}=[$event,$why];
+    $parent{$transcript}=$gene;
   }
   close(IN);
 }
