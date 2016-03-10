347096b3f62077033275b3aaf6d8740cb47a8e44
Bill Majoros <martiandna@gmail.com>
2016-03-10 14:33:55 -0500

    update

--- summarize-nmd.pl
+++ summarize-nmd.pl
@@ -3,14 +3,15 @@ use strict;
 use ProgramName;
 
 my $name=ProgramName::get();
-die "$name <infile>\n" unless @ARGV==1;
-my ($infile)=@ARGV;
+die "$name <infile> <min-sample-size>\n" unless @ARGV==2;
+my ($infile,$minN)=@ARGV;
 
 my ($success,$N);
 open(IN,$infile) || die $infile;
 while(<IN>) {
   chomp; my @fields=split; next unless @fields>=7;
   my ($transcriptID,$nmdMean,$funcMean,$nmdSD,$funcSD,$nmdN,$funcN)=@fields;
+  next unless $nmdN>=$minN && $funcN>=$minN;
   if($nmdMean<$funcMean) { ++$success }
   ++$N;
 }
