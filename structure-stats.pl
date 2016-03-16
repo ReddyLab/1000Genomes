4a0675b4a34a0275b0226f71da8ebdabc2b27eef
Bill Majoros <martiandna@gmail.com>
2016-03-16 14:39:26 -0400

    update

--- structure-stats.pl
+++ structure-stats.pl
@@ -1,3 +1,23 @@
 #!/usr/bin/perl
 use strict;
+use EssexParser;
+use EssexFBI;
+use ProgramName;
+
+my $name=ProgramName::get();
+die "$name <in.essex>\n" unless @ARGV==1;
+my ($infile)=@ARGV;
+
+my $parser=new EssexParser($infile);
+while(1) {
+  my $elem=$parser->nextElem();
+  last unless $elem;
+  my $report=new EssexFBI($elem);
+  my $status=$report->getStatusString();
+  # mapped/splicing-changes/no-transcript
+
+
+
+}
+
 
