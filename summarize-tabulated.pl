0f2ec3ea9877e1b126ad38b8eb1c41d31381e969
Bill Majoros <martiandna@gmail.com>
2016-03-17 10:23:01 -0400

    update

--- summarize-tabulated.pl
+++ summarize-tabulated.pl
@@ -6,7 +6,7 @@ my $THOUSAND="/home/bmajoros/1000G";
 my $ASSEMBLY="$THOUSAND/assembly";
 my $COMBINED="$ASSEMBLY/combined";
 
-my%hash;
+my (%hash,$altSum,$altN,$frameshiftLen,$frameshiftN);
 my @dirs=`ls $COMBINED`;
 my $slurmID=1;
 foreach my $subdir (@dirs) {
@@ -23,7 +23,10 @@ foreach my $key (@keys) {
   my ($mean,$stddev,$min,$max)=SummaryStats::roundedSummaryStats($array);
   print "$key\t$mean +/- $stddev ($min\-$max)\n";
 }
-
+my $meanAlt=$altSum/$altN;
+print "ALT_TRANSCRIPTS\tmean=$meanAlt\tsum=$altSum\tN=$altN\n";
+my $meanFrameshift=$frameshiftLen/$frameshiftN;
+print "FRAMESHIFT_LENGTHS\tmean=$meanFrameshift\tsum=$frameshiftLen\tN=$frameshiftN\n";
 
 sub process
 {
@@ -31,6 +34,17 @@ sub process
   open(IN,$infile) || die $infile;
   while(<IN>) {
     chomp;
+    if(/ALT_TRANSCRIPTS\s+(\S+)\s+(\S+)/) {
+      $altSum+=$1;
+      $altN+=$2;
+      next;
+    }
+    if(/GENES_FRAMESHIFT\s+(\S+)\s+(\S+)\s+(\S+)/) {
+      push @{$hash{"GENES_FRAMESHIFT"}},$1;
+      $frameshiftLen+=$2;
+      $frameshiftN+=$3;
+      next;
+    }
     if(/(\S+)\s+(\S+)/) { push @{$hash{$1}},$2 }
   }
   close(IN);
