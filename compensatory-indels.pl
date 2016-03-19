6337d856b3cee235f2add321457c8591cd8c20c5
Bill Majoros <martiandna@gmail.com>
2016-03-19 07:13:16 -0400

    update

--- compensatory-indels.pl
+++ compensatory-indels.pl
@@ -12,7 +12,7 @@ my $parser=new EssexParser($infile);
 while(1) {
   my $elem=$parser->nextElem();
   last unless $elem;
-  my $report=new EssexFBI($essexReportElem);
+  my $report=new EssexFBI($elem);
   next unless $report->getStatusString() eq "mapped";
   my $substrate=$report->getSubstrate();
   my $transcriptID=$report->getTranscriptID();
@@ -20,7 +20,30 @@ while(1) {
   my $transcript=$report->getMappedTranscript();
 
   my $indels=parseCigar($cigar);
-
+  my $numExons=$transcript->numExons();
+  for(my $i=0 ; $i<$numExons ; ++$i) {
+    my $exon=$transcript->getIthExon($i);
+    my $netIndel=0; my $hasFrameshiftIndel=0;
+    foreach my $indel (@$indels) {
+      if($exon->containsCoordinate($indel->{pos})) {
+	if($indel->{type} eq "I") { $netIndel+=$indel->{len} }
+	else { $netIndel-=$indel->{len} }
+	if($indel->{len}%3 != 0) { $hasFrameshiftIndel=1 }
+      }
+    }
+    if($hasFrameshiftIndel) {
+      my $mod=$netIndel%3;
+      next unless $mod==0;
+      #print "$netIndel\t$mod\n";
+      foreach my $indel (@$indels) {
+	if($exon->containsCoordinate($indel->{pos})) {
+	  print $indel->{len} . $indel->{type} . "\n";
+	}
+      }
+      print "===========================\n";
+    }
+  }
+  undef $indels;
 }
 
 
@@ -35,7 +58,7 @@ sub parseCigar
     chomp; my @fields=split; next unless @fields>=5;
     my ($refPos,$altPos,$len,$bp,$type)=@fields;
     $type=($type eq "insertion" ? "I" : "D");
-    my $rec={pos=>$pos,len=>$len,type=>$type};
+    my $rec={pos=>$altPos,len=>$len,type=>$type};
     push @$array,$rec;
   }
   close(CIGAR);
