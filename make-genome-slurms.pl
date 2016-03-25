562b420203efd69b6bb52f466c842ad5c16013df
Bill Majoros <martiandna@gmail.com>
2016-03-25 14:19:53 -0400

    update

--- make-genome-slurms.pl
+++ make-genome-slurms.pl
@@ -3,37 +3,26 @@ use strict;
 
 my $THOUSAND="/home/bmajoros/1000G";
 my $ASSEMBLY="$THOUSAND/assembly";
-my $COMBINED="$ASSEMBLY/combined";
-my $REF_FASTA="$COMBINED/ref/1.fasta";
-my $SLURM_DIR="$ASSEMBLY/fbi-slurms";
+my $FASTA="$ASSEMBLY/fasta";
+my $SLURM_DIR="$ASSEMBLY/genome-slurms";
 
-my @dirs=`ls $COMBINED`;
-my $slurmID=1;
-foreach my $subdir (@dirs) {
-  chomp $subdir;
-  next unless $subdir=~/^HG\d+$/ || $subdir=~/^NA\d+$/;
-  my $dir="$COMBINED/$subdir";
-  my $slurm="$SLURM_DIR/$slurmID.slurm";
-  unlink("$dir/1.essex") if -e "$dir/1.essex";
-  unlink("$dir/2.essex") if -e "$dir/2.essex";
+for(my $i=0 ; $i<100 ; ++$i) {
+  my $slurm="$SLURM_DIR/$i.slurm";
   open(OUT,">$slurm") || die $slurm;
   print OUT "#!/bin/bash
 #
-#SBATCH -J FBI$slurmID
-#SBATCH -o $SLURM_DIR/$slurmID.output
-#SBATCH -e $SLURM_DIR/$slurmID.output
-#SBATCH -A FBI$slurmID
-#SBATCH --mem 10000
+#SBATCH -J GENOME$i
+#SBATCH -o $SLURM_DIR/$i.output
+#SBATCH -e $SLURM_DIR/$i.output
+#SBATCH -A GENOME$i
+#SBATCH --mem 40000
 #
-cd $dir
+cd $FASTA
 
-/home/bmajoros/FBI/fbi.pl /home/bmajoros/1000G/FBI/model $REF_FASTA $dir/1.fasta /home/bmajoros/1000G/assembly/local-CDS-and-UTR.gff $dir/1.essex
-
-/home/bmajoros/FBI/fbi.pl /home/bmajoros/1000G/FBI/model $REF_FASTA $dir/2.fasta /home/bmajoros/1000G/assembly/local-CDS-and-UTR.gff $dir/2.essex
+/home/bmajoros/FBI/make-personal-genomes.pl /home/bmajoros/1000G/FBI/hg38/fbi.0-43.config /home/bmajoros/1000G/assembly/gene-set/genes$i.gff /home/bmajoros/1000G/assembly/fasta/$i
 
 ";
   close(OUT);
-  ++$slurmID;
 }
 
 
