12f5c85d5626dee25cd8de2c80e44ded684b7b36
Bill Majoros <martiandna@gmail.com>
2016-05-05 15:06:30 -0400

    update

---
+++ make-ethnic-genome-slurms.pl
@@ -0,0 +1,32 @@
+#!/usr/bin/perl
+use strict;
+
+my $THOUSAND="/home/bmajoros/1000G";
+my $ASSEMBLY="$THOUSAND/assembly";
+my $FASTA="$ASSEMBLY/fasta-ethnic";
+my $SLURM_DIR="$ASSEMBLY/ethnic-genome-slurms";
+
+for(my $i=0 ; $i<30 ; ++$i) {
+  my $slurm="$SLURM_DIR/$i.slurm";
+  open(OUT,">$slurm") || die $slurm;
+  print OUT "#!/bin/bash
+#
+#SBATCH -J ETHNIC$i
+#SBATCH -o $SLURM_DIR/$i.output
+#SBATCH -e $SLURM_DIR/$i.output
+#SBATCH -A ETHNIC$i
+#SBATCH --mem 40000
+#SBATCH -p new,all
+#
+module load htslib/1.2.1-gcb01
+module load kentUtils/v302-gcb01
+cd $FASTA
+
+/home/bmajoros/FBI/major-allele-genome.pl $THOUSAND/FBI/model/fbi.0-43.config $ASSEMBLY/gene-set/genes$i.gff $FASTA/$i $ASSEMBLY/pops.txt $ASSEMBLY/populations.txt
+
+";
+  close(OUT);
+}
+
+
+
--- make-gender-ancestry-file.pl
+++ make-gender-ancestry-file.pl
@@ -2,26 +2,39 @@
 use strict;
 
 my $VCF="/gpfs/fs0/data/common/1000_genomes/VCF/20130502";
+my $POP_INFO="$VCF/population-info.txt";
 my $ALL_ANCESTRIES="$VCF/ancestries.txt";
 my $GENDERS="/home/bmajoros/1000G/vcf/gender.txt";
+
+my %superpop;
+open(IN,$POP_INFO) || die $POP_INFO;
+while(<IN>) {
+  chomp;
+  my @fields=split/\t/; next unless @fields>=3;
+  my ($sub,$text,$super)=@fields;
+  $superpop{$sub}=$super;
+}
+close(IN);
 
 my %gender;
 open(IN,$GENDERS) || die $GENDERS;
 while(<IN>) {
   chomp;
-  my @fields=split; next unless @fields==2;
+  my @fields=split; next unless @fields>=2;
   my ($indiv,$gender)=@fields;
-  $gender{$invid}=$gender;
+  $gender{$indiv}=$gender;
 }
 close(IN);
 
 open(IN,$ALL_ANCESTRIES) || die $ALL_ANCESTRIES;
 while(<IN>) {
   chomp;
-  my @fields=split; next unless @fields==2;
+  my @fields=split; next unless @fields>=2;
   my ($indiv,$ancestry)=@fields;
+  my $super=$superpop{$ancestry};
   my $gender=$gender{$indiv};
-  print "$indiv\t$gender\t$ancestry\n";
+  next unless $gender;
+  print "$indiv\t$gender\t$ancestry\t$super\n";
 }
 close(IN);
 
