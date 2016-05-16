0727ef7bd6df39e5932b1cd0d662b7c69f2867fd
Bill Majoros <martiandna@gmail.com>
2016-05-16 14:42:52 -0400

    update

--- filter-by-gene-type.pl
+++ filter-by-gene-type.pl
@@ -3,20 +3,29 @@ use strict;
 use GffTranscriptReader;
 use ProgramName;
 
+my @keep=("protein_coding","pseudogene","lincRNA","processed_transcript","TR_V_pseudogene","polymorphic_pseudogene");
+my %keep;
+foreach my $type (@keep) {$keep{$type}=1}
+
 my $name=ProgramName::get();
-print "$name <infile.gff> <outfile.gff>\n" unless @ARGV==2;
+die "$name <infile.gff> <outfile.gff>\n" unless @ARGV==2;
 my ($infile,$outfile)=@ARGV;
 
+open(OUT,">$outfile") || die $outfile;
 my $reader=new GffTranscriptReader();
 my $transcripts=$reader->loadGFF($infile);
 my $n=@$transcripts;
 for(my $i=0 ; $i<$n ; ++$i) {
-  my $transcript=$transcripts->[$n];
+  my $transcript=$transcripts->[$i];
   my $extra=$transcript->parseExtraFields();
   my $hash=$transcript->hashExtraFields($extra);
   my $type=$hash->{"gene_type"};
-  print "$type\n";
-
+  next unless $keep{$type};
+  my $status=$hash->{"gene_status"};
+  next unless $status eq "KNOWN";
+  my $gff=$transcript->toGff();
+  print OUT $gff;
 }
+close(OUT);
 
 
