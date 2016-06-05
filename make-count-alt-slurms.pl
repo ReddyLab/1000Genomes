9a305b7d799d794ab2c94faa689926b619b41aae
Bill Majoros <martiandna@gmail.com>
2016-06-05 10:33:41 -0400

    update

--- make-count-alt-slurms.pl
+++ make-count-alt-slurms.pl
@@ -3,7 +3,7 @@ use strict;
 use SlurmWriter;
 
 my $THOUSAND="/home/bmajoros/1000G";
-my VCF="$THOUSAND/vcf";
+my $VCF="$THOUSAND/vcf";
 my $SLURM_DIR="$THOUSAND/assembly/count-alt-slurms";
 my $POPS="/home/bmajoros/1000G/assembly/populations.txt";
 my $CMD="/home/bmajoros/ethnic/count-alt-alleles";
@@ -12,9 +12,10 @@ my $writer=new SlurmWriter();
 my @FILES=`ls $VCF`;
 foreach my $file (@FILES) {
   chomp $file;
+  next unless $file=~/chr.*\.vcf\.gz$/;
   $writer->addCommand("$CMD $VCF/$file $POPS");
 }
 #$writer->mem(5000);
 $writer->setQueue("new");
-$writer->writeArrayScript($SLURM_DIR,"VCF",$SLURM_DIR);
+$writer->writeArrayScript($SLURM_DIR,"VCF",$SLURM_DIR,500);
 
