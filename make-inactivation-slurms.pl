38cda2fea063bfde9d4a7d495b5e23535564b17c
Bill Majoros <martiandna@gmail.com>
2016-06-06 12:14:00 -0400

    update

--- make-inactivation-slurms.pl
+++ make-inactivation-slurms.pl
@@ -1,33 +1,23 @@
 #!/usr/bin/perl
 use strict;
+use SlurmWriter;
 
 my $THOUSAND="/home/bmajoros/1000G";
 my $ASSEMBLY="$THOUSAND/assembly";
 my $COMBINED="$ASSEMBLY/combined";
 my $SLURM_DIR="$ASSEMBLY/inactivation-slurms";
 
+my $slurm=new SlurmWriter;
 my @dirs=`ls $COMBINED`;
-my $slurmID=1;
 foreach my $subdir (@dirs) {
   chomp $subdir;
   next unless $subdir=~/^HG\d+$/ || $subdir=~/^NA\d+$/;
   my $dir="$COMBINED/$subdir";
-  my $slurm="$SLURM_DIR/$slurmID.slurm";
-  open(OUT,">$slurm") || die $slurm;
-  print OUT "#!/bin/bash
-#
-#SBATCH -J LOF$slurmID
-#SBATCH -o $SLURM_DIR/$slurmID.output
-#SBATCH -e $SLURM_DIR/$slurmID.output
-#SBATCH -A LOF$slurmID
-#
-cd $dir
-$THOUSAND/src/essex-get-inactive.pl 1.essex > 1-inactivated.txt
-$THOUSAND/src/essex-get-inactive.pl 2.essex > 2-inactivated.txt
-";
-  close(OUT);
-  ++$slurmID;
+  $slurm->addCommand("$THOUSAND/src/essex-get-inactive.pl $dir/1-filtered.essex.old > $dir/1-inactivated.txt");
+  $slurm->addCommand("$THOUSAND/src/essex-get-inactive.pl $dir/2-filtered.essex.old > $dir/2-inactivated.txt");
 }
-
+$slurm->setQueue("new");
+$slurm->nice(500);
+$slurm->writeArrayScript($SLURM_DIR,"LOF",$SLURM_DIR,200);
 
 
