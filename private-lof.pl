3f8ed740f874a85cfa6e7d6dab10cefbad335d22
Bill Majoros <martiandna@gmail.com>
2016-06-07 10:24:38 -0400

    update

--- private-lof.pl
+++ private-lof.pl
@@ -17,13 +17,40 @@ close(IN);
 my (%ethnicGenes,%ethnicTranscripts);
 open(IN,$ETHNIC) || die $ETHNIC;
 while(<IN>) {
-  chomp; @fields=split;
+  chomp; my @fields=split;
   next unless @fields>=4 && $fields[3]=~/P=(\S+)/;
   my ($pop,$transcript,$gene)=@fields;
   $ethnicGenes{$pop}->{$gene}=1;
-  $ethnicTransripts{$pop}->{$transcript}=1;
+  $ethnicTranscripts{$pop}->{$transcript}=1;
 }
 close(IN);
+
+print "indiv\tshared\tprivate\n";
+my @dirs=`ls combined`;
+foreach my $dir (@dirs) {
+  chomp $dir;
+  next unless $dir=~/^HG/ || $dir=~/^NA/;
+  process("combined/$dir/1-inactivated.txt",$dir);
+  process("combined/$dir/2-inactivated.txt",$dir);
+}
+
+sub process
+{
+  my ($filename,$indiv)=@_;
+  my $private=0; my $shared=0;
+  my $pop=$pop{$indiv};
+  open(IN,$filename) || die $filename;
+  while(<IN>) {
+    chomp; my @fields=split; next unless @fields>=4;
+    my ($gene,$transcript,$fate,$why)=@fields;
+    if($ethnicTranscripts->{$pop}->{$transcript}) { ++$shared }
+    else { ++$private }
+  }
+  close(IN);
+  print "$indiv\t$shared\t$private\n";
+}
+
+
 
 
 
