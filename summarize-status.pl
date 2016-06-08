226dc0c0c7e8f1dae297e3a4830db1fe290d48f7
Bill Majoros <martiandna@gmail.com>
2016-06-08 11:55:33 -0400

    update

--- summarize-status.pl
+++ summarize-status.pl
@@ -10,6 +10,27 @@ my ($infile)=@ARGV;
 
 my $parser=new EssexParser($infile);
 while(1) {
+  my $root=$parser->nextElem();
+  last unless $root;
+  #my $fbi=new EssexFBI($root);
+  #my $transcriptID=$fbi->getTranscriptID();
+  #my $geneID=$fbi->getGeneID();
+  my $status=$root->findDescendent("status");
+  next unless $status;
+  next if $status->hasDescendentOrDatum("bad-annotation");
+  next unless $status->hasDescendentOrDatum("mapped");
+  my $numChildren=$status->numElements();
+  for(my $i=0 ; $i<$numChildren ; ++$i) {
+    my $child=$status->getIthElem($i);
+    if(EssexNode::isaNode($child)) {
+      my $tag=$child->getTag();
+      print "tag $tag\n";
+    }
+    else {
+      print "nontag $child\n";
+    }
+  }
+
 }
 
 
