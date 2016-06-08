9289b91621f9a6671b7868aa2b6d5c8ff1ddf55e
Bill Majoros <martiandna@gmail.com>
2016-06-08 12:12:19 -0400

    update

--- summarize-status.pl
+++ summarize-status.pl
@@ -3,34 +3,58 @@ use strict;
 use EssexParser;
 use EssexFBI;
 use ProgramName;
+$|=1;
+
+my $MAX_COUNT=10000;
 
 my $name=ProgramName::get();
 die "$name <infile>\n" unless @ARGV==1;
 my ($infile)=@ARGV;
 
+my (%hash,$sampleSize);
 my $parser=new EssexParser($infile);
 while(1) {
   my $root=$parser->nextElem();
   last unless $root;
-  #my $fbi=new EssexFBI($root);
+  my $fbi=new EssexFBI($root);
   #my $transcriptID=$fbi->getTranscriptID();
   #my $geneID=$fbi->getGeneID();
+  my $transcriptID=$root->getAttribute("transcript-ID");
   my $status=$root->findDescendent("status");
   next unless $status;
   next if $status->hasDescendentOrDatum("bad-annotation");
+  next if $status->hasDescendentOrDatum("too-many-vcf-errors");
   next unless $status->hasDescendentOrDatum("mapped");
   my $numChildren=$status->numElements();
+  my $splicingChanges;
   for(my $i=0 ; $i<$numChildren ; ++$i) {
     my $child=$status->getIthElem($i);
     if(EssexNode::isaNode($child)) {
       my $tag=$child->getTag();
-      print "tag $tag\n";
+      ++$hash{$tag};
+      #print "tag $tag\n" unless $tag eq "protein-differs";
     }
     else {
-      print "nontag $child\n";
+      #next if $child eq "noncoding";
+      ++$hash{$child};
+      #print "nontag $child\n" unless $child eq "mapped";
     }
+    #my $mapped=$status->findDescendent("mapped-transcript");
+    #if($mapped && $mapped->hasDescendentOrDatum("NMD")) {
   }
-
+  if($status->hasDescendentOrDatum("NMD")) {
+    #print "$transcriptID mapped NMD\n";
+    ++$hash{"mapped-NMD"};
+  }
+  if($splicingChanges && $fbi->allAltStructuresLOF()) {
+    ++$hash{"splicing-changes-all-LOF"}
+  }
+  ++$sampleSize;
+  if($MAX_COUNT>0 && $sampleSize>$MAX_COUNT) { last }
 }
 
-
+my @keys=keys %hash;
+foreach my $key (%hash) {
+  my $value=$hash{$key};
+  print "$key\t$value\n";
+}
