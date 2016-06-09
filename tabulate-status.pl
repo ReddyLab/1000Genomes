d33eb4a9ce7b6a04f93c4322396a98ca94424c19
Bill Majoros <martiandna@gmail.com>
2016-06-09 07:17:05 -0400

    update

--- tabulate-status.pl
+++ tabulate-status.pl
@@ -1,12 +1,13 @@
 #!/usr/bin/perl
 use strict;
+use SummaryStats;
 
 my $THOUSAND="/home/bmajoros/1000G";
 my $ASSEMBLY="$THOUSAND/assembly";
 my $COMBINED="$ASSEMBLY/combined";
 my $POP_FILE="$ASSEMBLY/populations.txt";
 
-my %pop;
+my (%pop,%attr);
 open(IN,$POP_FILE) || die "can't open file: $POP_FILE";
 while(<IN>) {
   chomp; my @fields=split; next unless @fields>=2;
@@ -15,14 +16,32 @@ while(<IN>) {
 }
 close(IN);
 
-my @files=`ls $COMBINED`;
-my $numFiles=@files;
-for(my $i=0 ; $i<$numFiles ; ++$i) {
-  my $file=$files[$i];
-  chomp $file;
-  $file=~// || die "can't parse filename: $file";
+my @dirs=`ls $COMBINED`;
+my $n=@dirs;
+for(my $i=0 ; $i<$n ; ++$i) {
+  my $dir=$dirs[$i];
+  chomp $dir;
+  $dir=~/([^\/]+)$/ || die $dir;
+  my $indiv=$1;
+  next unless $indiv=~/^HG\d+$/ || $indiv=~/^NA\d+$/;
+  my $pop=$pop{$indiv};
+  process("$COMBINED/$dir/1-status.txt",$pop);
+  process("$COMBINED/$dir/2-status.txt",$pop);
+}
+
+my @pops=keys %attr;
+my @attr=keys $attr{$pops[0]};
+
+
+
+sub process
+{
+  my ($file,$pop)=@_;
   open(IN,$file) || die "can't open file: $file";
   while(<IN>) {
+    chomp; my @fields=split; next unless @fields>=2;
+    my ($key,$value)=@fields;
+    push @{$attr{$pop}->{$key}},$value;
   }
   close(IN);
 }
