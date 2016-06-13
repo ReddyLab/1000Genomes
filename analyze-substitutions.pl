48ff081751c89046474256ece28d31f253358fd7
Bill Majoros <martiandna@gmail.com>
2016-06-13 13:54:01 -0400

    update

--- analyze-substitutions.pl
+++ analyze-substitutions.pl
@@ -3,34 +3,56 @@ use strict;
 use ProgramName;
 use SubstitutionMatrix;
 
+#my @keep=("AFR");
+my @keep=("AFR","SAS","EAS");
+my %keep; foreach my $k (@keep) { $keep{$k}=1 }
 my $MATRIX_FILE="/home/bmajoros/alignment/matrices/pam10";
 my $THOUSAND="/home/bmajoros/1000G";
 my $ASSEMBLY="$THOUSAND/assembly";
 my $COMBINED="$ASSEMBLY/combined";
+my $POP_FILE="$THOUSAND/assembly/populations.txt";
+
+my %pop;
+open(IN,$POP_FILE) || die "can't open file: $POP_FILE\n";
+while(<IN>) {
+  chomp; my @fields=split; next unless @fields>=2;
+  my ($id,$pop)=@fields;
+  $pop{$id}=$pop;
+}
+close(IN);
 
 #my ($refAndEthnicIdentical,$ethnicAndMappedIdentical);
-my ($ethnicLessExtreme,$refEthnicIdentical,$ethnicMappedIdentical,$sampleSize);
+my ($ethnicLessExtreme,$refEthnicIdentical,$ethnicMappedIdentical,$sampleSize,
+    $refMappedIdentical);
 my $M=new SubstitutionMatrix($MATRIX_FILE);
 my @dirs=`ls $COMBINED`;
 foreach my $subdir (@dirs) {
   chomp $subdir;
   next unless $subdir=~/^HG\d+$/ || $subdir=~/^NA\d+$/;
+  my $eth=$pop{$subdir};
+  die unless $eth;
+  #next if $eth eq "EUR";
+  #next unless $eth eq "AFR";
+  next unless $keep{$eth};
   my $dir="$COMBINED/$subdir";
+  next unless -e "$dir/1-substitutions.txt";
   process("$dir/1-substitutions.txt");
   process("$dir/2-substitutions.txt");
   my $percentLessExtreme=$ethnicLessExtreme/$sampleSize;
   my $percentRefEthnicIdentical=$refEthnicIdentical/$sampleSize;
   my $percentEthnicMappedIdentical=$ethnicMappedIdentical/$sampleSize;
+  my $percentRefMappedIdentical=$refMappedIdentical/$sampleSize;
   $percentLessExtreme=round($percentLessExtreme);
   $percentRefEthnicIdentical=round($percentRefEthnicIdentical);
   $percentEthnicMappedIdentical=round($percentEthnicMappedIdentical);
-  print "$percentLessExtreme\t$percentRefEthnicIdentical\t$percentEthnicMappedIdentical\tN=$sampleSize\n";
+  $percentRefMappedIdentical=round($percentRefMappedIdentical);
+  print "$percentLessExtreme ($percentRefMappedIdentical)\tREI=$percentRefEthnicIdentical\tEMI=$percentEthnicMappedIdentical\tN=$sampleSize\n";
 }
 
 sub round
 {
   my ($x)=@_;
-  return int($x*100+5/9)/100;
+  return int($x*1000+5/9)/1000;
 }
 
 sub process
@@ -42,7 +64,12 @@ sub process
     my ($indiv,$transcriptID,$ref,$ethnic,$mapped)=@fields;
     my $refToMapped=$M->lookup($ref,$mapped);
     my $ethnicToMapped=$M->lookup($ethnic,$mapped);
-    if($ethnicToMapped<$refToMapped) { ++$ethnicLessExtreme }
+    if($ethnicToMapped>$refToMapped) {
+      print "$ref => $ethnic => $mapped    $ethnicToMapped > $refToMapped\n"
+	unless $ref eq $mapped || $ethnic eq $mapped || $ethnicToMapped<-1;
+      ++$ethnicLessExtreme unless $ref eq $mapped || $ethnic eq $mapped;
+      if($ref eq $mapped) { ++$refMappedIdentical }
+    }
     if($ref eq $ethnic) { ++$refEthnicIdentical }
     if($ethnic eq $mapped) { ++$ethnicMappedIdentical }
     ++$sampleSize;
