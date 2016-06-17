10ec061b80eb2642983255cc4ef79f9843f449bc
Bill Majoros <martiandna@gmail.com>
2016-06-17 13:16:37 -0400

    update

--- make-indel-slurms.pl
+++ make-indel-slurms.pl
@@ -1,37 +1,23 @@
 #!/usr/bin/perl
 use strict;
+use SlurmWriter;
 
 my $THOUSAND="/home/bmajoros/1000G";
 my $ASSEMBLY="$THOUSAND/assembly";
 my $COMBINED="$ASSEMBLY/combined";
 my $SLURM_DIR="$ASSEMBLY/indel-slurms";
 
+my $slurm=new SlurmWriter;
 my @dirs=`ls $COMBINED`;
-my $slurmID=1;
 foreach my $subdir (@dirs) {
   chomp $subdir;
   next unless $subdir=~/^HG\d+$/ || $subdir=~/^NA\d+$/;
   my $dir="$COMBINED/$subdir";
-  my $slurm="$SLURM_DIR/$slurmID.slurm";
-  open(OUT,">$slurm") || die $slurm;
-  print OUT "#!/bin/bash
-#
-#SBATCH -J INDEL$slurmID
-#SBATCH -o $SLURM_DIR/$slurmID.output
-#SBATCH -e $SLURM_DIR/$slurmID.output
-#SBATCH -A INDEL$slurmID
-#
-cd $dir
-
-/home/bmajoros/1000G/src/compensatory-indels.pl $subdir $dir/1.essex > $dir/1-indels.txt
-
-/home/bmajoros/1000G/src/compensatory-indels.pl $subdir $dir/2.essex > $dir/2-indels.txt
-
-";
-  close(OUT);
-  ++$slurmID;
+  $slurm->addCommand("/home/bmajoros/1000G/src/compensatory-indels.pl $subdir $dir/1.essex > $dir/1-indels.txt");
+  $slurm->addCommand("/home/bmajoros/1000G/src/compensatory-indels.pl $subdir $dir/2.essex > $dir/2-indels.txt");
 }
+$slurm->setQueue("new,all");
+$slurm->writeArrayScript($SLURM_DIR,"INDEL",$SLURM_DIR,500);
 
-#SBATCH --mem 1000
 
 
