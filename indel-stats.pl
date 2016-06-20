c0bcc5a8e074e5c5e0e1e6fdd9c83b7e42a72e15
Bill Majoros <martiandna@gmail.com>
2016-06-20 10:41:18 -0400

    update

--- indel-stats.pl
+++ indel-stats.pl
@@ -4,7 +4,8 @@ use strict;
 my $THOUSAND="/home/bmajoros/1000G";
 my $COMBINED="$THOUSAND/assembly/combined";
 
-my (%indivsWithFrameshifts,%allIndivs);
+my (%indivsWithFrameshifts,%allIndivs,%genesWithFrameshifts,
+    %transcriptsWithFrameshifts);
 my @dirs=`ls $COMBINED`;
 foreach my $dir (@dirs) {
   chomp $dir;
@@ -20,7 +21,15 @@ foreach my $indiv (@indivs) {
   if($indivsWithFrameshifts{$indiv}) { ++$numIndivsWithFrameshifts }
 }
 my $percentIndivsWithFrameshifts=$numIndivsWithFrameshifts/$numIndivs;
-print "$percentIndivsWithFrameshifts = proportion of indivs with frameshifts\n";
+print "$percentIndivsWithFrameshifts = $numIndivsWithFrameshifts\/$numIndivs = proportion of indivs with frameshifts\n";
+my @genesWithFrameshifts=keys %genesWithFrameshifts;
+my $numGenesWithFrameshifts=@genesWithFrameshifts;
+my @transcriptsWithFrameshifts=keys %transcriptsWithFrameshifts;
+my $numTranscriptsWithFrameshifts=@transcriptsWithFrameshifts;
+print "$numGenesWithFrameshifts genes had a frameshift in at least one individual\n";
+print "$numTranscriptsWithFrameshifts transcripts had a frameshift in at least one individual\n";
+
+
 
 sub process
 {
@@ -30,10 +39,18 @@ sub process
     chomp; my @fields=split; next unless @fields>=5;
     if(/NOT CORRECTED/) {
       $indivsWithFrameshifts{$indiv}=1;
+      my $geneID=$fields[4];
+      my $transcriptID=$fields[5];
+      $genesWithFrameshifts{$geneID}=1;
+      $transcriptsWithFrameshifts{$transcriptID}=1;
 
     }
     else {
       $indivsWithFrameshifts{$indiv}=1;
+      my $geneID=$fields[2];
+      my $transcriptID=$fields[3];
+      $genesWithFrameshifts{$geneID}=1;
+      $transcriptsWithFrameshifts{$transcriptID}=1;
 
     }
   }
