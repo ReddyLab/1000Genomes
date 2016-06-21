6e22303311c66b57b9201c4df8629ffc2b7a67d0
Bill Majoros <martiandna@gmail.com>
2016-06-21 13:53:07 -0400

    update

--- basic-summary-stats.pl
+++ basic-summary-stats.pl
@@ -3,25 +3,25 @@ use strict;
 use ProgramName;
 use EssexParser;
 use EssexFBI;
+$|=1;
 
 my $name=ProgramName::get();
 die "$name <in.essex>\n" unless @ARGV==1;
 my ($infile)=@ARGV;
 
+my (%tooManyErrors);
 my $parser=new EssexParser($infile);
 while(1) {
   my $root=$parser->nextElem();
   last unless $root;
   my $fbi=new EssexFBI($root);
-  if($fbi->getStatusString() eq "mapped") {
-    my $variants=$root->pathQuery("mapped-transcript/variants");
-    if($variants) {
-      my $cdsVariants=$variants->findChild("CDS-variants");
-      my $numCdsVariants=$cdsVariants ? $cdsVariants->numElements() : 0;
-      my $utrVariants=$variants->findChild("UTR-variants");
-      my $numUtrVariants=$utrVariants ? $utrVariants->numElements() : 0;
-      print "$numCdsVariants\t$numUtrVariants\n";
-    }
+  my $transcriptID=$fbi->getTranscriptID();
+  my $geneID=$fbi->getGeneID();
+  my $status=$root->findChild("status");
+  my $statusString=$fbi->getStatusString();
+  if($statusString eq "mapped") { # includes too-many-vcf-errors
+    if($status->hasDescendentOrDatum("too-many-vcf-errors"))
+      { ++$tooManyErrors{$geneID} }
   }
   else { # splicing-changes/no-transcript/bad-annotation
   }
