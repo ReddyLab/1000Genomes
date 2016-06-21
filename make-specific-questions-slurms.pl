b2fc44109edde30987587139efd9ea568e42756c
Bill Majoros <martiandna@gmail.com>
2016-06-21 16:47:08 -0400

    update

--- make-specific-questions-slurms.pl
+++ make-specific-questions-slurms.pl
@@ -13,11 +13,11 @@ foreach my $subdir (@dirs) {
   chomp $subdir;
   next unless $subdir=~/^HG\d+$/ || $subdir=~/^NA\d+$/;
   my $dir="$COMBINED/$subdir";
-  $writer->addCommand("$THOUSAND/src/make-specific-questions-slurms.pl $dir/1.essex > $dir/1.specific-questions");
-  $writer->addCommand("$THOUSAND/src/make-specific-questions-slurms.pl $dir/2.essex > $dir/2.specific-questions");
+  $writer->addCommand("$THOUSAND/src/specific-questions.pl $dir/1.essex > $dir/1.specific-questions");
+  $writer->addCommand("$THOUSAND/src/specific-questions.pl $dir/2.essex > $dir/2.specific-questions");
 }
 #$writer->mem(5000);
-$writer->setQueue("all");
+$writer->setQueue("new,all");
 $writer->writeArrayScript($SLURM_DIR,"QUES",$SLURM_DIR,500);
 
 
