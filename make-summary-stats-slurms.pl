f65956d73f2fa49f02f264c08b87b7859dd94373
Bill Majoros <martiandna@gmail.com>
2016-06-21 15:12:57 -0400

    update

--- make-summary-stats-slurms.pl
+++ make-summary-stats-slurms.pl
@@ -5,7 +5,6 @@ use SlurmWriter;
 my $THOUSAND="/home/bmajoros/1000G";
 my $ASSEMBLY="$THOUSAND/assembly";
 my $COMBINED="$ASSEMBLY/combined";
-my $REF_FASTA="$COMBINED/ref/1.fasta";
 my $SLURM_DIR="$ASSEMBLY/summary-stats-slurms";
 
 my $writer=new SlurmWriter();
@@ -17,10 +16,9 @@ foreach my $subdir (@dirs) {
   $writer->addCommand("$THOUSAND/src/basic-summary-stats.pl $dir/1.essex > $dir/1.summary-stats");
   $writer->addCommand("$THOUSAND/src/basic-summary-stats.pl $dir/2.essex > $dir/2.summary-stats");
 }
-$writer->mem(5000);
+#$writer->mem(5000);
 $writer->setQueue("all");
-#$writer->writeScripts($NUM_JOBS,$SLURM_DIR,"FBI",$SLURM_DIR);
-$writer->writeArrayScript($SLURM_DIR,"FBI",$SLURM_DIR,500);
+$writer->writeArrayScript($SLURM_DIR,"STATS",$SLURM_DIR,500);
 
 
 
