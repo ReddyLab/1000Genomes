ec1da3627e59653ef9027744b8619b63d9b74c6b
Bill Majoros <martiandna@gmail.com>
2016-06-22 19:43:35 -0400

    update

--- lm-nmd.pl
+++ lm-nmd.pl
@@ -19,12 +19,43 @@ foreach my $indiv (@indivs) {
   my %alleleCounts;
   updateAlleleCounts("$dir/1-inactivated.txt",\%alleleCounts);
   updateAlleleCounts("$dir/2-inactivated.txt",\%alleleCounts);
+  invertAlleleCounts(\%alleleCounts);
   my $RNA_FILE="$dir/RNA/tab.txt";
   processRNA($RNA_FILE,\%alleleCounts);
 }
 
 
 
+#======================================================================
+sub invertAlleleCounts
+{
+  my ($hash)=@_;
+  my @keys=keys %$hash;
+  my $n=@keys;
+  for(my $i=0 ; $i<$n ; ++$i) {
+    my $key=$keys[$i];
+    $hash{$key}=2-$hash{$key};
+  }
+}
+#======================================================================
+sub updateAlleleCounts
+{
+  my ($filename,$hash)=@_;
+  open(IN,$filename) || die $filename;
+  while(<IN>) {
+    chomp; my @fields=split; next unless @fields>=4;
+    my ($gene,$transcript,$what,$why)=@fields;
+    next unless $what eq "NMD";
+    ++$hash->{$transcript};
+  }
+  close(IN);
+}
+#======================================================================
+sub processRNA
+{
+  my ($filename,$hash)=@_;
+
+}
 #======================================================================
 sub loadXY
 {
@@ -33,7 +64,7 @@ sub loadXY
   while(<IN>) {
     chomp; my @fields=split; next unless @fields>=3;
     my ($chr,$gene,$transcript)=@fields;
-    $hash{$gene}=1;
+    $hash->{$gene}=1;
   }
   close(IN);
 }
@@ -43,9 +74,9 @@ sub loadExpressed
   my ($filename,$hash)=@_;
   open(IN,$filename) || die "can't open file: $filename\n";
   while(<IN>) {
-    chomp; my @fields=split; next unless @fields>=3;
-    my ($gene,$transcript,$mean)=@fields;
-    $hash{$transcript}=$mean;
+    chomp; my @fields=split; next unless @fields>=4;
+    my ($gene,$transcript,$mean,$sampleSize)=@fields;
+    $hash->{$transcript}=$mean;
   }
   close(IN);
 }
