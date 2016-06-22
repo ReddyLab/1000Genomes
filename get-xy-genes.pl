51fc7a01867599fdbc31f5ab1d53ab4a0a2b6195
Bill Majoros <martiandna@gmail.com>
2016-06-22 18:00:22 -0400

    update

---
+++ get-xy-genes.pl
@@ -0,0 +1,24 @@
+#!/usr/bin/perl
+use strict;
+$|=1;
+
+my $THOUSAND="/home/bmajoros/1000G";
+my $INFILE="$THOUSAND/assembly/local-genes.gff";
+my $OUTFILE="$THOUSAND/assembly/xy.txt";
+
+my (%seen);
+open(IN,$INFILE) || die $INFILE;
+while(<IN>) {
+  chomp; my @fields=split; next unless @fields>=9;
+  my $chr=$fields[0];
+  next unless $chr eq "chrX" || $chr eq "chrY";
+  my ($transcript,$gene);
+  if(/transcript_id=([^;]+)/) { $transcript=$1 }
+  if(/gene_id=([^;]+)/) { $gene=$1 }
+  my $key="$gene $transcript";
+  next if $seen{$key};
+  $seen{$key}=1;
+  print "$chr\t$gene\t$transcript\n";
+}
+
+
