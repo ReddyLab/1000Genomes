5e9031d5019ad335300d1d658676ad3bad2a5af7
Bill Majoros <martiandna@gmail.com>
2016-06-22 18:43:27 -0400

    update

--- lm-nmd.pl
+++ lm-nmd.pl
@@ -1,18 +1,42 @@
 #!/usr/bin/perl
 use strict;
 
+# Globals
 my $THOUSAND="/home/bmajoros/1000G";
 my $ASSEMBLY="$THOUSAND/assembly";
-my $XY="$ASSEMBLY/xy.txt";
-my $EXPRESSED="$ASSEMBLY/expressed.txt";
+my %xy; # genes on X/Y chromosomes
+my %expressed; # transcripts expressed in LCLs
+loadXY("$ASSEMBLY/xy.txt",\%xy);
+loadExpressed("$ASSEMBLY/expressed.txt",\%expressed);
+
+
 
 
 
 #======================================================================
 sub loadXY
 {
+  my ($filename,$hash)=@_;
+  open(IN,$filename) || die "can't open file: $filename\n";
+  while(<IN>) {
+    chomp; my @fields=split; next unless @fields>=3;
+    my ($chr,$gene,$transcript)=@fields;
+    $hash{$gene}=1;
+  }
+  close(IN);
 }
 #======================================================================
+sub loadExpressed
+{
+  my ($filename,$hash)=@_;
+  open(IN,$filename) || die "can't open file: $filename\n";
+  while(<IN>) {
+    chomp; my @fields=split; next unless @fields>=2;
+    my ($gene,$transcript)=@fields;
+    $hash{$transcript}=1;
+  }
+  close(IN);
+}
 #======================================================================
 #======================================================================
 #======================================================================
