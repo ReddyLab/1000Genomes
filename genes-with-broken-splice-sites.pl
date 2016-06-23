926096a3db65ea0fcfc120ea0a9e9ca161ffc65e
Bill Majoros <martiandna@gmail.com>
2016-06-23 19:28:10 -0400

    update

--- genes-with-broken-splice-sites.pl
+++ genes-with-broken-splice-sites.pl
@@ -2,11 +2,13 @@
 use strict;
 use ProgramName;
 use EssexParser;
+$|=1;
 
 my $name=ProgramName::get();
 die "$name <indiv> <hap> <in.essex> <outfile>\n" unless @ARGV==4;
 my ($indiv,$hap,$infile,$outfile)=@ARGV;
 
+open(OUT,">$outfile") || die "can't write to file: $outfile";
 my $parser=new EssexParser($infile);
 while(1) {
   my $root=$parser->nextElem();
@@ -14,14 +16,14 @@ while(1) {
   my $status=$root->findChild("status");
   if($status->hasDescendentOrDatum("broken-donor") ||
      $status->hasDescendentOrDatum("broken-acceptor")) {
-    my $refTrans=$root->getChild("reference-transcript");
+    my $refTrans=$root->findChild("reference-transcript");
     my $chr=$refTrans->getAttribute("substrate");
     my $geneID=$root->getAttribute("gene-ID");
     my $transcriptID=$root->getAttribute("transcript-ID");
     print OUT "$indiv\t$hap\t$geneID\t$transcriptID\t$chr";
   }
 }
-
+close(OUT);
 
 
 
