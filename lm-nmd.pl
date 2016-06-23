2139f4071d5d8b64be5ff6df388dd2009c22ae0d
Bill Majoros <martiandna@gmail.com>
2016-06-23 06:51:07 -0400

    update

--- lm-nmd.pl
+++ lm-nmd.pl
@@ -16,12 +16,13 @@ foreach my $indiv (@indivs) {
   chomp $indiv;
   next unless $indiv=~/HG\d+/ || $indiv=~/NA\d+/;
   my $dir="$COMBINED/$indiv";
+  my $RNA_FILE="$dir/RNA/tab.txt";
+  next unless -e $RNA_FILE;
   my %alleleCounts;
   updateAlleleCounts("$dir/1-inactivated.txt",\%alleleCounts);
   updateAlleleCounts("$dir/2-inactivated.txt",\%alleleCounts);
-  invertAlleleCounts(\%alleleCounts);
-  my $RNA_FILE="$dir/RNA/tab.txt";
-  processRNA($RNA_FILE,\%alleleCounts,\%xy);
+  #invertAlleleCounts(\%alleleCounts);
+  processRNA($RNA_FILE,\%alleleCounts,\%xy,\%expressed);
 }
 
 
@@ -29,38 +30,46 @@ foreach my $indiv (@indivs) {
 #======================================================================
 sub processRNA
 {
-  my ($filename,$alleleCounts,$xy)=@_;
+  my ($filename,$alleleCounts,$xy,$expressed)=@_;
   open(IN,$filename) || die $filename;
   <IN>; # header line
   while(<IN>) {
     chomp; my @fields=split; next unless @fields>=7;
     my ($indiv,$allele,$gene,$transcript,$cov,$fpkm,$tpm)=@fields;
     next if($xy->{$gene}); # ignore sex chromosomes, due to ploidy issues
-    my $count=$alleleCounts->{$transcript}+0;
-    print "$count\t$fpkm\n";
+    my $mean=$expressed->{$transcript};
+    next unless $mean>0;
+    my $count=2-$alleleCounts->{$transcript}+0;
+    my $normalized=$fpkm/$mean;
+    print "$count\t$normalized\n";
   }
   close(IN);
 }
 #======================================================================
-sub invertAlleleCounts
-{
-  my ($hash)=@_;
-  my @keys=keys %$hash;
-  my $n=@keys;
-  for(my $i=0 ; $i<$n ; ++$i) {
-    my $key=$keys[$i];
-    $hash{$key}=2-$hash{$key};
-  }
-}
+#sub invertAlleleCounts
+#{
+#  my ($hash)=@_;
+#  my @keys=keys %$hash;
+#  my $n=@keys;
+#  for(my $i=0 ; $i<$n ; ++$i) {
+#    my $key=$keys[$i];
+#    print "BEFORE: " . $hash->{$key} . "\n";
+#    $hash->{$key}=2-$hash->{$key};
+#    print "\t\t\t" . $hash->{$key} . "\n";
+#  }
+#}
 #======================================================================
 sub updateAlleleCounts
 {
   my ($filename,$hash)=@_;
+  my %duplicates;
   open(IN,$filename) || die $filename;
   while(<IN>) {
     chomp; my @fields=split; next unless @fields>=4;
     my ($gene,$transcript,$what,$why)=@fields;
     next unless $what eq "NMD";
+    next if $duplicates{$transcript};
+    $duplicates{$transcript}=1;
     ++$hash->{$transcript};
   }
   close(IN);
