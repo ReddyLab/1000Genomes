d4101deef89e89b450100ec7b51f95fff6c43ae0
Bill Majoros <martiandna@gmail.com>
2016-06-24 06:16:04 -0400

    update

--- get-alt-genes.pl
+++ get-alt-genes.pl
@@ -9,7 +9,7 @@ my $OUTFILE="$ASSEMBLY/alt-genes.txt";
 open(OUT,">$OUTFILE") || die $OUTFILE;
 my @indivs=`ls $COMBINED`;
 foreach my $indiv (@indivs) {
-  chomp;
+  chomp $indiv;
   next unless $indiv=~/HG\d+/ || $indiv=~/NA\d+/;
   process("$COMBINED/$indiv/RNA/1and2.gff",$indiv);
 }
@@ -18,6 +18,7 @@ close(OUT);
 sub process
 {
   my ($filename,$indiv)=@_;
+  my %seen;
   open(IN,$filename) || die $filename;
   while(<IN>) {
     next unless/ALT\d_([^\"]+)/;
@@ -26,9 +27,9 @@ sub process
     my $substrate=$fields[0];
     $substrate=~/(\S+)_(\d)/ || die $substrate;
     my ($gene,$allele)=($1,$2);
-    my $key="$indiv\t$allele\t$transcript";
+    my $key="$allele\t$transcript";
     next if $seen{$key};
-    print OUT "$key\n";
+    print OUT "$indiv\t$allele\t$transcript\n";
     $seen{$key}=1;
   }
   close(IN);
