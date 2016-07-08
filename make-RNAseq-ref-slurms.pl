286eee47670c21da147223e8189c605a225afb29
Bill Majoros <martiandna@gmail.com>
2016-07-08 15:29:19 -0400

    update

--- make-RNAseq-ref-slurms.pl
+++ make-RNAseq-ref-slurms.pl
@@ -48,17 +48,17 @@ foreach my $ID (@IDs) {
 module load bowtie2/2.2.4-fasrc01
 module load tophat/2.0.13-gcb01
 
-cd $dir/RNA/ref
+cd /home/bmajoros/1000G/assembly/combined/ref/RNA
 
-tophat2 --output-dir $dir/RNA/ref --min-intron-length 30 --num-threads $CPUs --GTF $dir/RNA/ref/1.gff 1 $FASTQ/$rnaID\_1.fastq.gz $FASTQ/$rnaID\_2.fastq.gz
+tophat2 --output-dir $dir/RNA/ref --min-intron-length 30 --num-threads $CPUs --GTF $dir/RNA/ref/ref1.gff 1 $FASTQ/$rnaID\_1.fastq.gz $FASTQ/$rnaID\_2.fastq.gz
+
+cd $dir/RNA/ref
 
 /data/reddylab/software/samtools/samtools-1.1/samtools view accepted_hits.bam | /home/bmajoros/1000G/src/count-mapped-reads.pl > readcounts.txt
 
-/data/reddylab/software/stringtie/stringtie-1.2.1.Linux_x86_64/stringtie accepted_hits.bam -G $dir/RNA/ref/1.gff -o stringtie.gff -p $CPUs -C stringtie.coverage -A stringtie.abundance
+/data/reddylab/software/stringtie/stringtie-1.2.1.Linux_x86_64/stringtie accepted_hits.bam -G $dir/RNA/ref/ref1.gff -o stringtie-ref.gff -p $CPUs -C stringtie-ref.coverage -A stringtie-ref.abundance
 
 rm accepted_hits.bam
-
-
 
 ";
   close(OUT);
