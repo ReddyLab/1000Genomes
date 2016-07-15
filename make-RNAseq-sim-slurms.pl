d4b55d0b1ed2d66edd272f370309eabf2c66f71c
Bill Majoros <martiandna@gmail.com>
2016-07-15 15:46:11 -0400

    update

--- make-RNAseq-sim-slurms.pl
+++ make-RNAseq-sim-slurms.pl
@@ -6,7 +6,7 @@ my $MEMORY=40000;
 my $THOUSAND="/home/bmajoros/1000G";
 my $COMBINED="$THOUSAND/assembly/combined";
 my $RNA_LIST="$THOUSAND/assembly/id-map-parsed.txt";
-my $SLURMS="$THOUSAND/assembly/RNA-ref-slurms";
+my $SLURMS="$THOUSAND/assembly/RNA-sim-slurms";
 my $FASTQ="$THOUSAND/trim/output";
 
 my @IDs;
@@ -37,28 +37,33 @@ foreach my $ID (@IDs) {
   print OUT "#!/bin/bash
 #
 #SBATCH --get-user-env
-#SBATCH -J REF$slurmID
+#SBATCH -J SIM$slurmID
 #SBATCH -o $slurmID.output
 #SBATCH -e $slurmID.output
-#SBATCH -A REF$slurmID
+#SBATCH -A SIM$slurmID
 #SBATCH --mem $MEMORY
 #SBATCH --cpus-per-task=$CPUs
-#SBATCH -p all
+#SBATCH -p new
 #
 module load bowtie2/2.2.4-fasrc01
 module load tophat/2.0.13-gcb01
 
-cd /home/bmajoros/1000G/assembly/combined/ref/RNA
+cd $dir/RNA
+mkdir -p sim
+cd sim
 
-tophat2 --output-dir $dir/RNA/ref --min-intron-length 30 --num-threads $CPUs --GTF $dir/RNA/ref/ref1.gff 1 $FASTQ/$rnaID\_1.fastq.gz $FASTQ/$rnaID\_2.fastq.gz
+cp ../../random-1.gff ../../random-2.gff > sim.gff
 
-cd $dir/RNA/ref
+cat ../../1.fasta ../../2.fasta > 1and2.fa
 
-/data/reddylab/software/samtools/samtools-1.1/samtools view accepted_hits.bam | /home/bmajoros/1000G/src/count-mapped-reads.pl > readcounts.txt
+bowtie2-build 1and2.fa 1and2
 
-/data/reddylab/software/stringtie/stringtie-1.2.1.Linux_x86_64/stringtie accepted_hits.bam -G $dir/RNA/ref/ref1.gff -o stringtie-ref.gff -p $CPUs -C stringtie-ref.coverage -A stringtie-ref.abundance
+tophat2 --output-dir $dir/RNA/sim --min-intron-length 30 --num-threads $CPUs --GTF $dir/RNA/sim/sim.gff 1and2 $FASTQ/$rnaID\_1.fastq.gz $FASTQ/$rnaID\_2.fastq.gz
 
-rm accepted_hits.bam unmapped.bam
+/data/reddylab/software/stringtie/stringtie-1.2.1.Linux_x86_64/stringtie accepted_hits.bam -G $dir/RNA/sim/sim.gff -o stringtie.gff -p $CPUs -C stringtie.coverage -A stringtie.abundance
+
+rm *.bt2 accepted_hits.bam unmapped.bam
+rm 1and2.fa sim.gff
 
 ";
   close(OUT);
