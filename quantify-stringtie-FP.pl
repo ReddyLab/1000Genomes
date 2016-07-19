68f1976a5b6df22f0d8f3966e8db6e34f07cba62
Bill Majoros <martiandna@gmail.com>
2016-07-19 13:45:42 -0400

    update

--- quantify-stringtie-FP.pl
+++ quantify-stringtie-FP.pl
@@ -9,27 +9,48 @@ foreach my $indiv (@dirs) {
   chomp $indiv;
   next unless $indiv=~/HG\d+/ || $indiv=~/NA\d+/;
   next unless -e "$COMBINED/$indiv/RNA/stringtie.gff";
-  my (%blacklist,\%sims);
+  my (%blacklist,%sims);
   my $dir="$COMBINED/$indiv";
   my $sim="$dir/RNA/sim";
   loadBlacklist("$dir/random-1.blacklist",\%blacklist);
   loadBlacklist("$dir/random-2.blacklist",\%blacklist);
-  loadGFF("$dir/random-1.gff",\%sims,\%blacklist);
-  loadGFF("$dir/random-2.gff",\%sims,\%blacklist);
-
+  loadGFF("$dir/random-1.gff",1,\%sims,\%blacklist);
+  loadGFF("$dir/random-2.gff",2,\%sims,\%blacklist);
+  processRNA("$sim/tab.txt",\%sims,\%blacklist);
 }
 
 
 
-sub loadGFF
+sub processRNA
 {
   my ($filename,$sims,$blacklist)=@_;
+  open(IN,$filename) || die "can't open $filename";
+  <IN>; # header line
+  while(<IN>) {
+    chomp; my @fields=split; next unless @fields>=7;
+    my ($indiv,$allele,$gene,$transcript,$cov,$fpkm,$tpm)=@fields;
+    if($transcript=~/^SIM\d+_(\S+)/) {
+      my $id=$1;
+      my $key="$allele $transcript";
+      if($blacklist->{$key}) { next }
+      #if($blacklist->{$key}) { print "blacklist\t$transcript\t$fpkm\n" }
+      else { print "simulated\t$gene\t$id\t$fpkm\n" }
+    }
+    else { print "wildtype\t$indiv\t$gene\t$transcript\t$fpkm\n" }
+  }
+  close(IN);
+}
+
+
+sub loadGFF
+{
+  my ($filename,$allele,$sims,$blacklist)=@_;
   open(IN,$filename) || die "can't open $filename";
   while(<IN>) {
     chomp; my @fields=split; next unless @fields>=9;
     next unless $fields[1] eq "SIMULATION" && $fields[2] eq "transcript";
     $_=~/transcript_id \"([^\"]+)\";/ || die $_;
-    my $sim=$1;
+    my $sim="$allele $1";
     $sims->{$sim}=1;
   }
   close(IN);
