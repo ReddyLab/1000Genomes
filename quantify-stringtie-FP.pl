a779c95735593500d20b03829b950fa8aab9d675
Bill Majoros <martiandna@gmail.com>
2016-07-19 13:21:00 -0400

    update

--- quantify-stringtie-FP.pl
+++ quantify-stringtie-FP.pl
@@ -9,16 +9,31 @@ foreach my $indiv (@dirs) {
   chomp $indiv;
   next unless $indiv=~/HG\d+/ || $indiv=~/NA\d+/;
   next unless -e "$COMBINED/$indiv/RNA/stringtie.gff";
-  my %blacklist;
+  my (%blacklist,\%sims);
   my $dir="$COMBINED/$indiv";
   my $sim="$dir/RNA/sim";
   loadBlacklist("$dir/random-1.blacklist",\%blacklist);
   loadBlacklist("$dir/random-2.blacklist",\%blacklist);
-
+  loadGFF("$dir/random-1.gff",\%sims,\%blacklist);
+  loadGFF("$dir/random-2.gff",\%sims,\%blacklist);
 
 }
 
 
+
+sub loadGFF
+{
+  my ($filename,$sims,$blacklist)=@_;
+  open(IN,$filename) || die "can't open $filename";
+  while(<IN>) {
+    chomp; my @fields=split; next unless @fields>=9;
+    next unless $fields[1] eq "SIMULATION" && $fields[2] eq "transcript";
+    $_=~/transcript_id \"([^\"]+)\";/ || die $_;
+    my $sim=$1;
+    $sims->{$sim}=1;
+  }
+  close(IN);
+}
 
 
 sub loadBlacklist
