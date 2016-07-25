3d8522f405011d4d392749cbe1c6c92de7d74f7c
Bill Majoros <martiandna@gmail.com>
2016-07-25 08:52:37 -0400

    update

---
+++ check-done.pl
@@ -0,0 +1,46 @@
+#!/usr/bin/perl
+use strict;
+use ProgramName;
+
+my $name=ProgramName::get();
+die "$name <slurm-directory>\n" unless @ARGV==1;
+my ($slurmDir)=@ARGV;
+
+my $dir="$slurmDir/outputs";
+die "$dir does not exist\n" unless -e $dir;
+my @commands=`ls $slurmDir/command*.sh`;
+my $N=@commands;
+
+my @done;
+for(my $i=1 ; $i<=$N ; ++$i) { $done[$i]=0 }
+for(my $i=1 ; $i<=$N ; ++$i) {
+  my $file="$dir/$i.output";
+  if(-e $file && done($file)) { $done[$i]=1 }
+}
+my $first=1;
+for(my $i=1 ; $i<=$N ; ++$i) {
+  next if $done[$i];
+  my $j; for($j=$i+1 ; $j<=$N && !$done[$j]; ++$j) {}
+  if(!$first) { print "," }
+  my $end=$j-1;
+  if($i==$end) { print "$i" }
+  else { print "$i\-$end" }
+  $i=$j;
+  $first=0;
+}
+print "\n";
+
+
+sub done
+{
+  my ($filename)=@_;
+  open(IN,"tail $filename|") || die $filename;
+  while(<IN>) {
+    if(/\[done\]/) { close(IN); return 1 }
+  }
+  close(IN);
+  return 0;
+}
+
+
+
