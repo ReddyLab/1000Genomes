e3cfb9b9bbce79e95ce35ca16d0268cf62abef59
Bill Majoros <martiandna@gmail.com>
2016-07-26 15:59:05 -0400

    update

--- summarize-blind-diff.pl
+++ summarize-blind-diff.pl
@@ -4,7 +4,8 @@ use strict;
 my $THOUSAND="/home/bmajoros/1000G";
 my $COMBINED="$THOUSAND/assembly/combined";
 
-my ($totalFound,$totalNotFound,$uniqueFound,$uniqueNotFound,%seen);
+my ($totalFound,$totalNotFound,$uniqueFound,$uniqueNotFound,%seen,
+    $geneFound,$geneNotFound,%geneSeen);
 my @dirs=`ls $COMBINED`;
 foreach my $subdir (@dirs) {
   chomp $subdir;
@@ -20,14 +21,17 @@ print "$ratio ($totalFound\/$total) expressed ALTs were found by StringTie witho
 my $total=$uniqueFound+$uniqueNotFound;
 my $ratio=$uniqueFound/$total;
 print "$ratio ($uniqueFound\/$total) transcripts with expressed ALTs were found by StringTie without annotation\n";
+my $total=$geneFound+$geneNotFound;
+my $ratio=$geneFound/$total;
+print "$ratio ($geneFound\/$total) genes with expressed ALTs were found by StringTie without annotation\n";
 
 
 sub process {
   my ($infile)=@_;
   open(IN,$infile) || die "can't open $infile";
   while(<IN>) {
-    chomp; my @fields=split; next unless @fields>=2;
-    my ($transcriptID,$found)=@fields;
+    chomp; my @fields=split; next unless @fields>=3;
+    my ($geneID,$transcriptID,$found)=@fields;
     if($transcriptID=~/ALT\d+_(\S+)/) { $transcriptID=$1 }
     if($found) { ++$totalFound }
     else { ++$totalNotFound }
@@ -35,6 +39,10 @@ sub process {
     if($found) { ++$uniqueFound }
     else { ++$uniqueNotFound }
     $seen{$transcriptID}=1;
+    next if $geneSeen{$geneID};
+    if($found) { ++$geneFound }
+    else { ++$geneNotFound }
+    $geneSeen{$geneID}=1;
   }
   close(IN);
 }
