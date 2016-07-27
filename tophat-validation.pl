b70fab1b1738c45dcb64cac87849ee43154fd408
Bill Majoros <martiandna@gmail.com>
2016-07-27 15:29:09 -0400

    update

--- tophat-validation.pl
+++ tophat-validation.pl
@@ -4,18 +4,30 @@ use ProgramName;
 use GffTranscriptReader;
 
 my $name=ProgramName::get();
-die "$name <in.gff> <junctions.bed>\n" unless @ARGV==2;
-my ($gffFile,$junctionsFile)=@ARGV;
+die "$name <in.gff> <junctions.bed> <prefix=ALT|SIM>\n" unless @ARGV==3;
+my ($gffFile,$junctionsFile,$PREFIX)=@ARGV;
 
 # Load the TopHat junctions file
 my $introns=parseTophat($junctionsFile);
 
-# Process the GFF
+# Load the GFF file
 my $reader=new GffTranscriptReader();
 my $transcripts=$reader->loadGFF($gffFile);
-my $transcriptHash=hashTranscriptIDs($transripts);
+my $transcriptHash=hashTranscriptIDs($transcripts);
 
+# Process all ALT/SIM structures
+my $n=@$transcripts;
+for(my $i=0 ; $i<$n ; ++$i) {
+  my $transcript=$transcripts->[$i];
+  my $id=$transcript->getTranscriptId();
+  next unless $id=~/(\S\S\S)\d+_(\S+)/;
+  next unless $1 eq $PREFIX;
+  my $parentID=$2;
+  my $parent=$transcriptHash->{$parentID};
+  die $parentID unless $parent;
+}
 
+# Terminate
 print STDERR "[done]\n";
 
 
