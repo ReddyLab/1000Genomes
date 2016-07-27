00ce2779fd822fa86c9b7d5d76c9d824457d058d
Bill Majoros <martiandna@gmail.com>
2016-07-27 17:11:40 -0400

    update

--- tophat-validation.pl
+++ tophat-validation.pl
@@ -4,8 +4,13 @@ use ProgramName;
 use GffTranscriptReader;
 
 my $name=ProgramName::get();
-die "$name <in.gff> <junctions.bed> <prefix=ALT|SIM>\n" unless @ARGV==3;
-my ($gffFile,$junctionsFile,$PREFIX)=@ARGV;
+die "$name <in.gff> <allele> <junctions.bed> <prefix=ALT|SIM> <blacklist> <nmd-list>\n" unless @ARGV==6;
+my ($gffFile,$allele,$junctionsFile,$PREFIX,$BLACKLIST,$NMD)=@ARGV;
+
+# Load blacklist (ALTs or SIMs that happen to match an existing isoform)
+my %blacklist;
+loadBlacklist($BLACKLIST,\%blacklist);
+loadNMD($NMD,\%blacklist,$PREFIX);
 
 # Load the TopHat junctions file
 my $introns=parseTophat($junctionsFile);
@@ -18,11 +23,13 @@ my $transcriptHash=hashTranscriptIDs($transcripts);
 
 # Process all ALT/SIM structures
 my $n=@$transcripts;
+my $sampleSize=0; my $numFound=0;
 for(my $i=0 ; $i<$n ; ++$i) {
   my $transcript=$transcripts->[$i];
   my $id=$transcript->getTranscriptId();
   next unless $id=~/(\S\S\S)\d+_(\S+)/;
   next unless $1 eq $PREFIX;
+  next if $blacklist{$id};
   my $parentID=$2;
   my $parent=$transcriptHash->{$parentID};
   die $parentID unless $parent;
@@ -30,10 +37,14 @@ for(my $i=0 ; $i<$n ; ++$i) {
   my $geneID=$transcript->getGeneId();
   my $geneIntrons=$introns->{$geneID};
   if(!$geneIntrons) { $geneIntrons=[] }
+  my $found;
   if($transcript->numExons()<$parent->numExons())
-    { exonSkipping($transcript,$parent,$geneIntrons) }
-  else { crypticSplicing($transcript,$parent,$geneIntrons) }
+    { $found=exonSkipping($transcript,$parent,$geneIntrons) }
+  else { $found=crypticSplicing($transcript,$parent,$geneIntrons) }
+  ++$sampleSize;
+  $numFound+=$found;
 }
+print "$numFound out of $sampleSize\n";
 
 # Terminate
 print STDERR "[done]\n";
@@ -76,30 +87,32 @@ sub exonSkipping {
       if($begin==$intronBegin && $end==$intronEnd) {
 	my $transcriptID=$child->getTranscriptId();
 	print "SKIPPING $transcriptID $strand $begin $end\n";
-	last;
+	return 1;
       }
     }
     last;
   }
+  return 0;
 }
 
 
 sub crypticSplicing {
   my ($child,$parent,$junctions)=@_;
   my $numChildExons=$child->numExons(); my $numParentExons=$parent->numExons();
-  die "$numChildExons vs $numParentExons"
-    unless $numChildExons==$numParentExons;
+  if($numChildExons>$numParentExons) { return }
+    #my $parentGff=$parent->toGff(); my $childGff=$child->toGff();
+    #die "unequal num exons: $numChildExons vs $numParentExons\nPARENT:\n$parentGff\nCHILD:\n$childGff\n"; }
   for(my $i=0 ; $i<$numParentExons ; ++$i) {
-    if($i>=$numChildExons) {
-      my $parentGff=$parent->toGff(); my $childGff=$child->toGff();
-      die "skipped exon not found\n$numParentExons exons vs. $numChildExons exons\nparent:\n$parentGff\nchild:\n$childGff"; }
+    #if($i>=$numChildExons) {
+    #  my $parentGff=$parent->toGff(); my $childGff=$child->toGff();
+    #  die "skipped exon not found\n$numParentExons exons vs. $numChildExons exons\nparent:\n$parentGff\nchild:\n$childGff"; }
     my $exon=$parent->getIthExon($i);
     my $begin=$exon->getBegin(); my $end=$exon->getEnd();
     my $childExon=$child->getIthExon($i);
     if($childExon->getBegin()==$begin && $childExon->getEnd()==$end) { next }
-    if($i<1 || $i>=$numChildExons) {
-      my $parentGff=$parent->toGff(); my $childGff=$child->toGff();
-      die "i=$i vs. numChildExons=$numChildExons\nPARENT:\n$parentGff\nCHILD:\n$childGff\n" }
+    if($i<1 || $i>=$numChildExons) { next }
+      #my $parentGff=$parent->toGff(); my $childGff=$child->toGff();
+      #die "i=$i vs. numChildExons=$numChildExons\nPARENT:\n$parentGff\nCHILD:\n$childGff\n" }
     my $strand=$child->getStrand();
     my ($intronBegin,$intronEnd);
     if($strand eq "+") {
@@ -123,11 +136,12 @@ sub crypticSplicing {
       if($begin==$intronBegin && $end==$intronEnd) {
 	my $transcriptID=$child->getTranscriptId();
 	print "CRYPTIC $transcriptID $strand $begin $end\n";
-	last;
+	return 1;
       }
     }
     last;
   }
+  return 0;
 }
 
 
@@ -168,19 +182,58 @@ sub coalesceExons {
   my $n=@$transcripts;
   for(my $i=0 ; $i<$n ; ++$i) {
     my $transcript=$transcripts->[$i];
+    my $strand=$transcript->getStrand();
     my $exons=$transcript->{exons};
+    my $UTR=$transcript->{UTR};
+    die if @$UTR>0;
     my $n=@$exons;
     my $changes;
     for(my $i=0 ; $i<$n-1 ; ++$i) {
       my $this=$exons->[$i]; my $next=$exons->[$i+1];
-      if($this->{end}==$next->{begin}) {
-	$this->{end}=$next->{end};
-	splice(@$exons,$i+1,1);
-	$changes=1;
+      if($strand eq "+" ) {
+	if($this->{end}==$next->{begin}) {
+	  $this->{end}=$next->{end};
+	  splice(@$exons,$i+1,1);
+	  --$n;
+	  $changes=1; }
+      } else {
+	if($this->{begin}==$next->{end}) {
+	  $this->{begin}=$next->{begin};
+	  splice(@$exons,$i+1,1);
+	  --$n;
+	  $changes=1; }
       }
     }
     if($changes) {$transcript->{exons}=$exons}
   }
 }
 
+
+sub loadBlacklist {
+  my ($filename,$hash)=@_;
+  open(IN,$filename) || die $filename;
+  while(<IN>) {
+    chomp; my @fields=split; next unless @fields>=6;
+    my ($indiv,$allele,$chr,$gene,$transcript,$ALT)=@fields;
+    $ALT=~/\S\S\S\d+_(\S+)/ || die $ALT;
+    #$ALT=$1;
+    my $id="$ALT\_$allele";
+    #print "black: [$id]\n";
+    $hash->{$id}=1;
+  }
+  close(IN);
+}
+
+sub loadNMD {
+  my ($filename,$hash,$prefix)=@_;
+  open(IN,$filename) || die $filename;
+  while(<IN>) {
+    chomp; my @fields=split; next unless @fields>=7;
+    my ($indiv,$allele,$chr,$gene,$transcript,$ALT,$status)=@fields;
+    $ALT=~/\S\S\S(\d+_\S+)/ || die $ALT; # The prefix may be wrong...that's ok
+    my $id="$prefix$1\_$allele";
+    if($status eq "NMD") { $hash->{$id}=1 }
+  }
+  close(IN);
+}
 
