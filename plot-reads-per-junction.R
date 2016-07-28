ac3fe574c1d42ac39c3eeafcace3001dd3e27684
Bill Majoros <martiandna@gmail.com>
2016-07-28 15:16:12 -0400

    update

---
+++ plot-reads-per-junction.R
@@ -0,0 +1,21 @@
+#!/usr/bin/Rscript --vanilla
+
+resolution <- 2;
+infile1 <- "neg-reads-per-junction.txt";
+infile2 <- "pos-reads-per-junction.txt";
+x <- "Reads per junction";
+y <- "Relative frequency";
+outfile <- "reads-per-junction.pdf"
+
+data1 <- read.table(infile1);
+data2 <- read.table(infile2);
+
+pdf(outfile);
+par(mfrow=c(2,1))
+h1 <- hist(data1$V1,breaks=seq(0,90,resolution),plot=F);
+h2 <- hist(data2$V1,breaks=seq(0,90,resolution),plot=F);
+h1$density = h1$counts/sum(h1$counts);
+h2$density = h2$counts/sum(h2$counts);
+plot(h1,col=rgb(0.1,0.1,0.1,1/4),xlim=c(0,90),ylim=c(0,1),xlab=x,ylab=y,freq=F,main="Reads per junction in simulated isoforms");
+plot(h2,col=rgb(0.5,0.5,0.5,1/4),xlim=c(0,90),ylim=c(0,1),xlab=x,ylab=y,freq=F,main="Reads per junction in ICE isoforms");
+dev.off();
---
+++ plot-supported.R
@@ -0,0 +1,21 @@
+#!/usr/bin/Rscript --vanilla
+
+resolution <- 0.01;
+infile1 <- "neg-supported.txt";
+infile2 <- "pos-supported.txt";
+x <- "Proportion of isoforms supported";
+y <- "Relative frequency";
+outfile <- "support.pdf"
+
+data1 <- read.table(infile1);
+data2 <- read.table(infile2);
+
+pdf(outfile);
+par(mfrow=c(2,1))
+h1 <- hist(data1$V1,breaks=seq(0,0.5,resolution),plot=F);
+h2 <- hist(data2$V1,breaks=seq(0,0.5,resolution),plot=F);
+h1$density = h1$counts/sum(h1$counts);
+h2$density = h2$counts/sum(h2$counts);
+plot(h1,col=rgb(0.1,0.1,0.1,1/4),xlim=c(0,0.5),ylim=c(0,0.22),xlab=x,ylab=y,freq=F,main="Simulated isoforms supported by at least one read");
+plot(h2,col=rgb(0.5,0.5,0.5,1/4),xlim=c(0,0.5),ylim=c(0,0.22),xlab=x,ylab=y,freq=F,main="ICE isoforms supported by at least one read");
+dev.off();
