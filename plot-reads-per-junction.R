c12a9f94e31534145eef684eab60cb9d3a64cc80
Bill Majoros <martiandna@gmail.com>
2016-07-28 15:23:01 -0400

    update

--- plot-reads-per-junction.R
+++ plot-reads-per-junction.R
@@ -1,6 +1,6 @@
 #!/usr/bin/Rscript --vanilla
 
-resolution <- 2;
+resolution <- 1;
 infile1 <- "neg-reads-per-junction.txt";
 infile2 <- "pos-reads-per-junction.txt";
 x <- "Reads per junction";
@@ -16,6 +16,6 @@ h1 <- hist(data1$V1,breaks=seq(0,90,resolution),plot=F);
 h2 <- hist(data2$V1,breaks=seq(0,90,resolution),plot=F);
 h1$density = h1$counts/sum(h1$counts);
 h2$density = h2$counts/sum(h2$counts);
-plot(h1,col=rgb(0.1,0.1,0.1,1/4),xlim=c(0,90),ylim=c(0,1),xlab=x,ylab=y,freq=F,main="Reads per junction in simulated isoforms");
-plot(h2,col=rgb(0.5,0.5,0.5,1/4),xlim=c(0,90),ylim=c(0,1),xlab=x,ylab=y,freq=F,main="Reads per junction in ICE isoforms");
+plot(h1,col=rgb(0.1,0.1,0.1,1/4),xlim=c(0,90),ylim=c(0,0.6),xlab=x,ylab=y,freq=F,main="Reads per junction in simulated isoforms");
+plot(h2,col=rgb(0.5,0.5,0.5,1/4),xlim=c(0,90),ylim=c(0,0.12),xlab=x,ylab=y,freq=F,main="Reads per junction in ICE isoforms");
 dev.off();
