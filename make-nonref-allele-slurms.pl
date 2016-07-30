74f3f01e5eacf1c300369b88a77322c692b27e9f
Bill Majoros <martiandna@gmail.com>
2016-07-30 09:21:35 -0400

    update

---
+++ make-nonref-allele-slurms.pl
@@ -0,0 +1,26 @@
+#!/usr/bin/perl
+use strict;
+use SlurmWriter;
+
+my $THOUSAND="/home/bmajoros/1000G";
+my $ASSEMBLY="$THOUSAND/assembly";
+my $SLURM_DIR="$ASSEMBLY/nonref-allele-slurms";
+my $VCF="$THOUSAND/vcf";
+my $PROGRAM="$THOUSAND/src/get-nonref-allele-counts.pl";
+
+my $writer=new SlurmWriter();
+my @VCF=`ls $VCF`;
+foreach my $file (@VCF) {
+  chomp $file;
+  next unless $file=~/ALL\.chr([^\.]+)\..*vcf.gz/;
+  my $chr=$1;
+  next if $chr eq "X" || $chr eq "Y";
+  my $path="$VCF/$file";
+  my $outfile="$OUTDIR/chr$chr.txt";
+  $writer->addCommand("$PROGRAM $path > $outfile");
+}
+$writer->mem(5000);
+$writer->setQueue("new,all");
+$writer->writeArrayScript($SLURM_DIR,"VCF",$SLURM_DIR,500);
+
+
