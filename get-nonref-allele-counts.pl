4b47b9a60e2c86137db00f60ce4a2d699442b2c0
Bill Majoros <martiandna@gmail.com>
2016-07-30 09:16:35 -0400

    update

--- get-nonref-allele-counts.pl
+++ get-nonref-allele-counts.pl
@@ -1,13 +1,16 @@
 #!/usr/bin/perl
 use strict;
 use ProgramName;
+$|=1;
+
+my $MAX_VARIANTS=-1;
 
 my $name=ProgramName::get();
-die "$name <in.vcf>\n" unless @ARGV==1;
+die "$name <in.vcf.gz>\n" unless @ARGV==1;
 my ($infile)=@ARGV;
 
-my (@indexToID,%counts);
-open(IN,$infile) || die "can't open $infile";
+my (@indexToID,%counts,$variantNum);
+open(IN,"cat $infile | gunzip |") || die "can't open $infile";
 while(<IN>) {
   next if(/^##/);
   chomp; my @fields=split/\t/,$_; next unless @fields>=9;
@@ -19,15 +22,26 @@ while(<IN>) {
     }
   }
   else {
+    ++$variantNum;
+    last unless $MAX_VARIANTS<0 || $variantNum<$MAX_VARIANTS;
     for(my $i=9 ; $i<$n ; ++$i) {
       my $G=$fields[$i];
       my $id=$indexToID[$i];
       my @fields=split/\|/,$G;
-      my $count;
+      my $count=0;
       foreach my $field (@fields) { if($field>0) {++$count} }
       $counts{$id}+=$count;
+      #my $debug=$counts{$id};
+      #print "$id : $debug + $count\n";
     }
   }
 }
 close(IN);
+
+my @keys=keys %counts;
+foreach my $indiv (@keys) {
+  my $count=$counts{$indiv};
+  print "$indiv\t$count\n";
+}
+
 
