6df44bec3fe2748e1bc39670355a663c72d5f1b8
Bill Majoros <martiandna@gmail.com>
2016-07-30 07:19:19 -0400

    update

---
+++ analyze-broken-RVIS.pl
@@ -0,0 +1,16 @@
+#!/usr/bin/perl
+use strict;
+
+my $RVIS="/home/bmajoros/intolerance/RVIS/RVIS.txt";
+my $broken="/home/bmajoros/1000G/assembly/broken-genes.txt";
+
+my %RVIS;
+open(IN,$RVIS) || die $RVIS;
+while(<IN>) {
+  chomp;
+  
+}
+close(IN);
+
+
+
--- parse-hgnc.pl
+++ parse-hgnc.pl
@@ -7,11 +7,14 @@ open(OUT,">$ENSEMBL/parsed.txt") || die;
 open(IN,"$ENSEMBL/download.txt") || die;
 <IN>; # header
 while(<IN>) {
-  chomp; my @fields=split; next unless @fields>=2;
+  chomp; my @fields=split/\t/; next unless @fields>=2;
   my ($num,$approved,$previous,$synonyms,$ensembl)=@fields;
+  next unless $ensembl=~/\S/;
   if($approved=~/(\S)~withdrawn/) { $approved=$1 }
-  #if($approved=~/\S/) { print "$ensembl\t$approved\n" }
+  if($approved=~/\S/) { print "$ensembl\t$approved\n" }
   @fields=split/[, ]+/,$previous;
+  foreach my $field (@fields) { print "$ensembl\t$field\n" }
+  @fields=split/[, ]+/,$synonyms;
   foreach my $field (@fields) { print "$ensembl\t$field\n" }
 }
 close(IN);
