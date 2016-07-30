60893caaffc45784a8631af2c1a71a660a383590
Bill Majoros <martiandna@gmail.com>
2016-07-30 07:50:37 -0400

    update

--- analyze-broken-RVIS.pl
+++ analyze-broken-RVIS.pl
@@ -1,6 +1,7 @@
 #!/usr/bin/perl
 use strict;
 
+my $MIN_COUNT=100;
 my $RVIS="/home/bmajoros/intolerance/RVIS/RVIS.txt";
 my $BROKEN="/home/bmajoros/1000G/assembly/broken.txt";
 my $NAMES="/home/bmajoros/ensembl/gene-names/combined.txt";
@@ -25,21 +26,25 @@ while(<IN>) {
 }
 close(IN);
 
-my %seen;
+my %counts;
 open(IN,$BROKEN) || die $BROKEN;
 while(<IN>) {
-  chomp; my @fields=split; next unless @fields>=3;
-  my ($gene,$transcript,$chr)=@fields;
-  next if $seen{$gene};
-  $seen{$gene}=1;
+  chomp; my @fields=split; next unless @fields>=5;
+  my ($indiv,$allele,$gene,$transcript,$chr)=@fields;
   next if $chr eq "chrX" || $chr eq "chrY";
   if($gene=~/(\S+)\./) { $gene=$1 }
-  #print "looking up $gene.\n";
   my $rvis=$RVIS{$gene};
   next unless defined $rvis;
-  print "$gene\t$rvis\n";
+  ++$counts{$gene};
 }
 close(IN);
 
-
+my @genes=keys %counts;
+foreach my $gene (@genes) {
+  my $count=$counts{$gene};
+  my $rvis=$RVIS{$gene};
+  die unless defined $rvis;
+  next unless $count>=$MIN_COUNT;
+  print "$count\t$rvis\n";
+}
 
