9bfcfa56d7c81616e8a2cd6812e8d6d843ec1357
Bill Majoros <martiandna@gmail.com>
2016-08-02 14:15:36 -0400

    update

--- plot-RVIS.R
+++ plot-RVIS.R
@@ -5,20 +5,21 @@ if(length(args)!=7) {
   cat("usage: infile.txt color min-x max-x num-bins x-label outfile.pdf\n");
   q(status=1)
 }
-infile <- args[1];
-c <- args[2];
-minX <- as.numeric(args[3]);
-maxX <- as.numeric(args[4]);
-nbreaks <- as.numeric(args[5]);
-xlabel <- args[6];
-outfile <- args[7];
+infile <- "broken-rvis-percentile.txt"
+c <- rgb(0.1,0.1,0.1,1/4)
+minX <- 0
+maxX <- 10
+nbreaks <- 10
+xlabel <- "RVIS percentile"
+outfile <- "RVIS.pdf"
+title <- "Distribution of RVIS percentiles"
 
 pdf(outfile);
 data <- read.table(infile);
 if(nbreaks>0) {
-  hist(data$V1,xlim=c(minX,maxX),xlab=xlabel,col=c,breaks=nbreaks);
+  hist(data$V1,xlim=c(minX,maxX),xlab=xlabel,col=c,breaks=nbreaks,main=title);
 } else {
-  hist(data$V1,xlim=c(minX,maxX),xlab=xlabel,col=c);
+  hist(data$V1,xlim=c(minX,maxX),xlab=xlabel,col=c,main=title);
 }
 dev.off();
 
