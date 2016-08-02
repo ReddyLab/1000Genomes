eb9ab465c78444507fee69ce5bdf1222d1c4257d
Bill Majoros <martiandna@gmail.com>
2016-08-02 19:02:18 -0400

    update

--- noncoding-to-coding.pl
+++ noncoding-to-coding.pl
@@ -7,6 +7,7 @@ my $name=ProgramName::get();
 die "$name <in.essex>\n" unless @ARGV==1;
 my ($infile)=@ARGV;
 
+print "reason\tGeneID\tTranscriptID\tOldOrfLen\tNewOrfLen\tRefStartScore\tAltStartScore\tStartScoreCutoff\n";
 my $parser=new EssexParser($infile);
 while(1) {
   my $root=$parser->nextElem(); last unless $root;
@@ -18,5 +19,8 @@ while(1) {
   my $orflenNode=$change->findChild("ORF-length");
   my $oldOrfLen=$orflenNode->getIthElem(0);
   my $newOrfLen=$orflenNode->getIthElem(2);
-  
+  my $refStartScore=$change->getAttribute("ref-start-score");
+  my $altStartScore=$change->getAttribute("alt-start-score");
+  my $startScoreCutoff=$change->getAttribute("start-score-cutoff");
+  print "$reason\t$geneID\t$transcriptID\t$oldOrfLen\t$newOrfLen\t$refStartScore\t$altStartScore\t$startScoreCutoff\n";
 }
