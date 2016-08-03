109509daff6f5143aafe08b16e364b6656424271
Bill Majoros <martiandna@gmail.com>
2016-08-03 13:15:56 -0400

    update

--- make-RVIS-baseline2.pl
+++ make-RVIS-baseline2.pl
@@ -9,15 +9,28 @@ my $GFF="/home/bmajoros/1000G/assembly/local-genes.gff";
 
 my $reader=new GffTranscriptReader;
 my $transcripts=$reader->loadGFF($GFF);
+my $numTranscripts=@$transcripts;
 
+my (%byNumExons,%numExons);
+for(my $i=0 ; $i<$numTranscripts ; ++$i) {
+  my $transcript=$transcripts->[$i];
+  my $numExons=$transcript->numExons();
+  push @{$byNumExons{$numExons}},$transcript;
+  $numExons{$transcript->getTransriptId()}=$numExons;
+}
 
+# Substitute broken genes for randomly selected ones with the desired trait
 open(IN,$BROKEN) || die $BROKEN;
 while(<IN>) {
   chomp; my @fields=split; next unless @fields>=5;
-  my ($indiv,$allele,$gene,$transcript,$chr)=@fields;
-  next if $chr eq "chrX" || $chr eq "chrY";
-  if($gene=~/(\S+)\./) { $gene=$1 }
-  $alleles{$gene}->{$indiv}->{$allele}=1;
+  my ($indiv,$allele,$gene,$transcriptID,$chr)=@fields;
+  my $numExons=$numExons{$transcriptID};
+  my $array=$byNumExons{$numExons};
+  my $n=$array ? @$array : 0;
+  next unless $n>0;
+  my $random=$array->[int(rand($n))];
+  my $id=$random->getTranscriptId();
+  print "$indiv\t$allele\t$gene\t$id\t$chr\n";
 }
 close(IN);
 
