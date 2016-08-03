73a3bbac11875642cae52e505e0271b5193aea00
Bill Majoros <martiandna@gmail.com>
2016-08-03 09:46:28 -0400

    update

--- compensatory-figure-segregation.pl
+++ compensatory-figure-segregation.pl
@@ -24,21 +24,25 @@ for(my $i=0 ; $i<@rs ; ++$i) {
   my $rs1=$rs[$i];
   for(my $j=$i+1 ; $j<@rs ; ++$j) {
     my $rs2=$rs[$j];
-    my $coseg=coseg($genotypes{$rs1},$genotypes{$rs2});
-    print "$rs1\t$rs2\t$coseg\n";
+    coseg($rs1,$genotypes{$rs1},$rs2,$genotypes{$rs2});
   }
 }
 
 sub coseg {
-  my ($A1,$A2)=@_;
+  my ($rs1,$A1,$rs2,$A2)=@_;
   my $sum=0;
   my $L=@$A1;
+  my $hasFirst=0; my $hasSecond=0; my $hasBoth=0; my $hasEither=0;
   for(my $i=0 ; $i<$L ; ++$i) {
     my $pair1=$A1->[$i]; my $pair2=$A2->[$i];
     my $alt1=$pair1->[0] || $pair1->[1];
-    my $alt1=$pair1->[0] || $pair1->[1];
+    my $alt2=$pair2->[0] || $pair2->[1];
+    if($alt1) { ++$hasFirst }
+    if($alt2) { ++$hasSecond }
+    if($alt1 && $alt2) { ++$hasBoth }
+    if($alt1 || $alt2) { ++$hasEither }
   }
-  my $score=$sum/$L;
-  return $score;
+  my $jaccard=$hasBoth/$hasEither;
+  print "$rs1=$hasFirst\t$rs2=$hasSecond\tboth=$hasBoth\teither=$hasEither\tJaccard=$jaccard\n";
 }
 
