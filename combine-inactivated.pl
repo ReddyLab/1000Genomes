8babc5b4d6a88db4c19b74d4624f5f8bb2f41d23
Bill Majoros <martiandna@gmail.com>
2016-08-08 11:00:58 -0400

    update

---
+++ combine-inactivated.pl
@@ -0,0 +1,34 @@
+#!/usr/bin/perl
+use strict;
+
+my $THOUSAND="/home/bmajoros/1000G";
+my $ASSEMBLY="$THOUSAND/assembly";
+my $COMBINED="$ASSEMBLY/combined";
+my %xy; # genes on X/Y chromosomes
+loadXY("$ASSEMBLY/xy.txt",\%xy);
+
+my @dirs=`ls $COMBINED`;
+foreach my $indiv (@dirs) {
+  chomp $indiv;
+  next unless $indiv=~/HG\d+/ || $indiv=~/NA\d+/;
+  process("$COMBINED/$indiv/1-inactivated.txt",$indiv,1);
+  process("$COMBINED/$indiv/2-inactivated.txt",$indiv,2);
+}
+
+
+sub process {
+  my ($filename,$indiv,$allele)=@_;
+  open(IN,$filename) || die $filename;
+  while(<IN>) {
+    chomp; my @fields=split; next unless @fields>=4;
+    my ($gene,$transcript,$what,$why)=@fields;
+    print "$indiv\t$allele\t$gene\t$transcript";
+  }
+  close(IN);
+}
+
+
+
+
+
+
