34f81df569c71293276877d0ddbe5d20f2d6f2a6
Bill Majoros <martiandna@gmail.com>
2016-08-08 17:56:52 -0400

    update

--- RVIS-num-isoforms.pl
+++ RVIS-num-isoforms.pl
@@ -4,10 +4,11 @@ use GffTranscriptReader;
 
 my $THOUSAND="/home/bmajoros/1000G/assembly";
 my $CARSON="$THOUSAND/carson.txt";
+my $GFF="$THOUSAND/local-genes.gff";
 
 open(BG,">isoform-counts-bkg.txt") || die;
 my $reader=new GffTranscriptReader;
-my $allGenes=$reader->loadGenes($filename);
+my $allGenes=$reader->loadGenes($GFF);
 my %geneHash;
 my $numGenes=@$allGenes;
 for(my $i=0 ; $i<$numGenes ; ++$i) {
@@ -19,12 +20,12 @@ for(my $i=0 ; $i<$numGenes ; ++$i) {
 }
 close(BG);
 
-open(FG<">isoform-counts-RVIS.txt") || die;
+open(FG,">isoform-counts-RVIS.txt") || die;
 open(IN,$CARSON) || die $CARSON;
 while(<IN>) {
   chomp; my @fields=split; next unless @fields>=3;
   my ($geneID,$count,$RVIS)=@fields;
-  my $gene=$geneHash{$gendID};
+  my $gene=$geneHash{$geneID};
   my $numTrans=$gene->getNumTranscripts();
   print FG "$numTrans\n";
 }
