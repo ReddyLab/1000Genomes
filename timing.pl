e8db2258d5792854cb57701fe7ba403197be3d23
Bill Majoros <martiandna@gmail.com>
2016-08-09 15:57:34 -0400

    update

--- timing.pl
+++ timing.pl
@@ -6,9 +6,9 @@ my $OUTPUTS="$THOUSAND/fbi-slurms/outputs/old";
 
 my @files=`ls $OUTPUTS`;
 foreach my $file (@files) {
-  chomp; next unless $file=~/.output/;
+  chomp $file; next unless $file=~/.output/;
   my @dates;
-  open(IN,$file) || die $file;
+  open(IN,"grep EDT $OUTPUTS/$file |") || die $file;
   while(<IN>) {
     if(/\S+ \S+ (\d+) (\d+):(\d+):(\d+) EDT 2016/) {
       my ($day,$hour,$min,$sec)=($1,$2,$3,$4);
@@ -20,6 +20,11 @@ foreach my $file (@files) {
   next unless $numDates==2;
   my ($day1,$hour1)=@{$dates[0]};
   my ($day2,$hour2)=@{$dates[1]};
+  my $elapsed;
+  if($day1==$day2) { $elapsed=$hour2-$hour1 }
+  elsif($day2>$day1) {$elapsed=24-$hour1+$hour2+24*($day2-$day1-1)}
+  else { die "$day1 $hour1 - $day2 $hour2\n" }
+  print "$elapsed hours\n";
 }
 
 
