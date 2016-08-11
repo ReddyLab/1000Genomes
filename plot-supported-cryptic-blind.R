960d838e7ed7de0540b9d0cf3907428f007b4b0d
Bill Majoros <martiandna@gmail.com>
2016-08-11 12:56:11 -0400

    update

--- plot-supported-cryptic-blind.R
+++ plot-supported-cryptic-blind.R
@@ -1,22 +1,80 @@
 #!/usr/bin/Rscript --vanilla
 
+
+int.hist = function(x,ylab="Frequency",...) {
+    barplot(table(factor(x,levels=min(x):max(x))),space=0,xaxt="n",ylab=ylab,...);axis(1)
+}
+
+#====
+
+outfile <- "support-cryptic-blind-density.pdf"
+
+pdf(outfile);
+par(mfrow=c(3,1))
+
+infile <- "alt-struct-counts.txt";
+x <- "Number of alternate structures predicted";
+y <- "Frequency"
+data <- read.table(infile);
+h1 <- int.hist(data$V1,col="white");
+minX <- min(data$V1); maxX <- max(data$V1);
+#plot(h1,xlab=x,ylab=y,xlim=c(minX,maxX),main="");
+
+#====
+
 resolution <- 0.01;
 infile1 <- "supported-cryptic-blind.txt";
 infile2 <- "supported-cryptic.txt";
 x <- "Proportion of cryptic-site isoforms supported by at least one spliced read";
-y <- "Frequency";
-outfile <- "support-cryptic-blind.pdf"
-
+y <- "Density"
 data1 <- read.table(infile1);
 data2 <- read.table(infile2);
+h1 <- density(data1$V1,from=0);
+h2 <- density(data2$V1,from=0);
+min1 <- min(data1$V1); max1 <- max(data1$V1);
+min2 <- min(data2$V1); max2 <- max(data2$V1);
+minX <- min(min1,min2); maxX <- max(max1,max2);
+plot(h1,col="blue",xlab=x,ylab=y,xlim=c(minX,maxX),lwd=2,main="");
+lines(h2,col="red",lwd=2);
+legend("topright",legend=c("Blind","Informed"),
+       lty=c(1,1),lwd=c(2,2),col=c("blue","red"));
 
-pdf(outfile);
-par(mfrow=c(2,1))
-h1 <- hist(data1$V1,breaks=seq(0,0.35,resolution),plot=F);
-h2 <- hist(data2$V1,breaks=seq(0,0.35,resolution),plot=F);
-#h1$density = h1$counts/sum(h1$counts);
-#h2$density = h2$counts/sum(h2$counts);
-plot(h1,col=rgb(0.1,0.1,0.1,1/4),xlim=c(0,0.35),xlab=x,ylab=y,freq=F,main="Supported cryptic-site isoforms when ICE predictions\nare not provided to TopHat");
-plot(h2,col=rgb(0.1,0.1,0.1,1/4),xlim=c(0,0.35),xlab=x,ylab=y,freq=F,main="Supported cryptic-site isoforms when ICE predictions\nare provided to TopHat");
+#====
+infile1 <- "sensitivity-histogram-cryptic.txt";
+infile2 <- "proposal-histogram-cryptic.txt";
+x <- "Proportion of cryptic-site isoforms assigned nonzero FPKM";
+y <- "Density";
+data1 <- read.table(infile1);
+data2 <- read.table(infile2);
+h1 <- density(data1$V1);
+h2 <- density(data2$V1);
+min1 <- min(data1$V1); max1 <- max(data1$V1);
+min2 <- min(data2$V1); max2 <- max(data2$V1);
+minX <- min(min1,min2); maxX <- max(max1,max2);
+plot(h1,col="blue",xlim=c(minX,maxX),ylim=c(0,50),xlab=x,ylab=y,main="",lwd=2);
+lines(h2,col="red",xlim=c(minX,maxX),lwd=2);
+legend("topright",legend=c("Blind","Informed"),
+       lty=c(1,1),lwd=c(2,2),col=c("blue","red"));
+
+#====
+
+infile1 <- "supported-cryptic-sim.txt";
+infile2 <- "supported-cryptic.txt";
+x <- "Proportion of cryptic-site isoforms supported by at least one read";
+y <- "Density";
+data1 <- read.table(infile1);
+data2 <- read.table(infile2);
+h1 <- density(data1$V1);
+h2 <- density(data2$V1);
+min1 <- min(data1$V1); max1 <- max(data1$V1);
+min2 <- min(data2$V1); max2 <- max(data2$V1);
+minX <- min(min1,min2); maxX <- max(max1,max2);
+plot(h1,col="blue",xlim=c(minX,maxX),xlab=x,ylab=y,main="",lwd=2);
+lines(h2,col="red",xlim=c(minX,maxX),lwd=2);
+legend("topright",legend=c("Simulated","Predicted"),
+       lty=c(1,1),lwd=c(2,2),col=c("blue","red"));
+
+#====
 dev.off();
+
 
