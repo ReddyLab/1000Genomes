2cef921f8e27dc412becc110f0d8403140d90d2e
Bill Majoros <martiandna@gmail.com>
2016-08-11 08:00:43 -0400

    update

--- plot-effect-sizes.R
+++ plot-effect-sizes.R
@@ -6,16 +6,14 @@ minX <- -12;
 maxX <- 2;
 nbreaks <- 50;
 xlabel <- "Log2 effect size"
-outfile <- "effect-sizes.pdf"
+outfile <- "effect-sizes-density.pdf"
 title <- "Distribution of log2 effect size"
 
 pdf(outfile);
 data <- read.table(infile);
-if(nbreaks>0) {
-  hist(data$V1,xlim=c(minX,maxX),xlab=xlabel,col=c,breaks=nbreaks,main=title);
-} else {
-  hist(data$V1,xlim=c(minX,maxX),xlab=xlabel,col=c,main=title);
-}
+d <- density(data$V1);
+plot(d,xlim=c(minX,maxX),xlab=xlabel,main=title);
+abline(v=-1,lty=2)
 dev.off();
 
 
