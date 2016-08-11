fa388174b75c46aae4dea800644ab530e7505c61
Bill Majoros <martiandna@gmail.com>
2016-08-11 07:53:16 -0400

    update

--- plot-betas.R
+++ plot-betas.R
@@ -1,18 +1,20 @@
 #!/usr/bin/Rscript --vanilla
 
+outfile <- "betas-transformed.pdf"
 infile <- "betas.txt"
 c <- "black"
 x <- "FPKM threshold"
-y <- "Beta"
+y <- "Mean FPKM proportion"
+title <- "Reduction in expression as a function of FPKM threshold"
 minX <- 0
 maxX <- 20
 minY <- 0
 maxY <- 1
-outfile <- "betas.pdf"
-title <- "Beta as a function of FPKM threshold"
 
 pdf(outfile);
 data <- read.table(infile);
+data$V2 <- 0.5 ^ (2*data$V2);
+#plot(data,col=c,xlab=x,ylab=y,xlim=c(minX,maxX),ylim=c(minY,maxY),pch=16,main=title);
 plot(data,col=c,xlab=x,ylab=y,xlim=c(minX,maxX),ylim=c(minY,maxY),pch=16,
      main=title);
 dev.off();
