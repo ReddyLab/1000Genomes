64b78be1cad3e85bf719f34a1ceb9e092ecd35ea
Bill Majoros <martiandna@gmail.com>
2016-08-16 15:23:26 -0400

    update

--- summarize-crypskip.pl
+++ summarize-crypskip.pl
@@ -3,15 +3,37 @@ use strict;
 
 my $THOUSAND="/home/bmajoros/1000G";
 my $COMBINED="$THOUSAND/assembly/combined";
-my $SLURMS="$THOUSAND/assembly/predict-crypskip-slurms";
-my $PROGRAM="$THOUSAND/src/predict-crypskip.pl";
 
-my $slurm=new SlurmWriter;
+my ($CS,$Cs,$cS,$cs);
 my @dirs=`ls $COMBINED`;
-foreach my $subdir (@dirs) {
-  chomp $subdir;
-  next unless $subdir=~/^HG\d+$/ || $subdir=~/^NA\d+$/;
-  my $dir="$COMBINED/$subdir";
+foreach my $indiv (@dirs) {
+  chomp $indiv;
+  next unless $indiv=~/^HG\d+$/ || $indiv=~/^NA\d+$/;
+  my $dir="$COMBINED/$indiv";
   next unless -e "$dir/RNA/stringtie.gff";
+  process("$dir/1.crypskip-counts");
+  process("$dir/2.crypskip-counts");
+}
 
+print "CS=$CS Cs=$Cs cS=$cS cs=$cs\n";
+System("fisher-exact-test.R $CS $Cs $cS $cs");
+
+
+sub process {
+  my ($infile)=@_;
+  open(IN,$infile) || die $infile;
+  while(<IN>) {
+    chomp; my @fields=split; next unless @fields>=2;
+    my ($cryptic,$skipping)=@fields;
+    if($cryptic>0) {
+      if($skipping>0) {	++$CS }
+      else { ++Cs }
+    }
+    else {
+      if($skipping>0) {	++$cS }
+      else { ++cs }
+    }
+  }
+  close(IN);
+}
 
