bee1abee80beada973aa25baed13149a69ca2b7f
Bill Majoros <martiandna@gmail.com>
2016-08-16 12:32:45 -0400

    update

--- classify-alt-crypskip.pl
+++ classify-alt-crypskip.pl
@@ -2,6 +2,7 @@
 use strict;
 use ProgramName;
 use EssexParser;
+$|=1;
 
 my $name=ProgramName::get();
 die "$name <haplotype> <in.essex>\n" unless @ARGV==2;
@@ -12,15 +13,25 @@ while(1) {
   my $root=$parser->nextElem();
   last unless $root;
   my $altStructsNode=
-    $root->pathQuery("report/status/alternate-structures/transcript");
-  next unless $altStructNode;
-  my $altStructs=$altStructNode->findChildren("transcript");
+    $root->pathQuery("report/status/alternate-structures");
+  next unless $altStructsNode;
+  my $altStructs=$altStructsNode->findChildren("transcript");
   my $altNum=0;
   my $transcriptID=$root->getAttribute("transcript-ID");
   foreach my $transcript (@$altStructs) {
     my $altID="ALT$altNum\_$transcriptID\_$hap";
     my $change=$transcript->getAttribute("structure-change");
-    print "$altID\t$change\n";
+    if($change eq "cryptic-site") {
+      my $brokenSiteNode=$root->pathQuery("report/status/broken-donor");
+      if(!$brokenSiteNode)
+	{ $brokenSiteNode=$root->pathQuery("report/status/broken-acceptor") }
+      my $brokenPos=$brokenSiteNode->getIthElem(0);
+      my $crypticSiteNode=$transcript->findChild("cryptic-site");
+      my $crypticPos=$crypticSiteNode->getIthElem(1);
+      my $distance=abs($crypticPos-$brokenPos);
+      print "$altID\t$change\t$distance\n";
+    }
+    else { print "$altID\t$change\n" }
     ++$altNum;
   }
 }
