be49fdbfea842cc5c0ab0705b4c1d9ff232a0b52
Bill Majoros <martiandna@gmail.com>
2016-09-01 18:55:20 -0400

    update

--- simulate-tetraploid.pl
+++ simulate-tetraploid.pl
@@ -3,9 +3,10 @@ use strict;
 
 my $BASE="/home/bmajoros/1000G/assembly/test";
 my $INFILE="$BASE/human-chr10.vcf.gz";
+my $OUTFILE="$BASE/tetra.vcf";
 
 open(IN,"cat $INFILE | gunzip |") || die;
-open(OUT,">tetra.vcf.gz") || die;
+open(OUT,">$OUTFILE") || die;
 while(<IN>) {
   if(/#CHROM/) {
     my @fields=split;
@@ -17,13 +18,13 @@ while(<IN>) {
   }
   elsif(/#/) { print OUT }
   else {
+    my @fields=split; next unless @fields>=10;
     for(my $i=0 ; $i<10 ; ++$i) {
       print OUT $fields[$i];
       if($i<9) { print OUT "\t" }
     }
-    my $a=int(rand(2)); print "$a|";
-    my $b=int(rand(2)); print "$b";
-    print OUT "\n";
+    my $a=int(rand(2)); my $b=int(rand(2)); 
+    print OUT "|$a|$b\n";
   }
 }
 close(OUT);
