8e7c925ef0d29ba254a5619cece69981aa8049d9
Bill Majoros <martiandna@gmail.com>
2016-09-14 14:42:38 -0400

    update

--- crypskip-fpkm.pl
+++ crypskip-fpkm.pl
@@ -1,12 +1,14 @@
 #!/usr/bin/perl
 use strict;
 
+my $READS=0; # 1 = use spliced read counts (TopHat), 0 = use FPKM (StringTie)
+
 my $MIN_FPKM=1;
 my $MIN_SAMPLE_SIZE=30;
+my $MAX_COPIES=100;
 my $BASE="/home/bmajoros/1000G/assembly";
 my $CRYPSKIP="$BASE/crypskip.txt";
-#my $RNA="$BASE/rna.txt";
-my $RNA="$BASE/crypskip-counts.txt";
+my $RNA=$READS ? "$BASE/crypskip-counts.txt" : "$BASE/rna.txt";
 
 my (%rna,%rnaByTranscript,%sampleSize,%seen);
 open(IN,$RNA) || die $RNA;
@@ -16,34 +18,32 @@ while(<IN>) {
   next if $indiv eq "indiv"; # header
   #next unless $nmd eq "OK";
   my $key="$indiv $allele";
-  #if($transcript=~/ALT\d+_(\S+)/) { $transcript=$1 }
-  #print "key=\"$key\" transcript=\"$transcript\" FPKM=\"$FPKM\"\n";
   next if $transcript eq ".";
   if($transcript=~/(ALT\d+_\S+)_\d+/) { $transcript=$1 }
-
   my $baseID=$transcript;
   if($transcript=~/ALT\d+_(\S+)/) { $baseID=$1 }
-  next if $seen{$baseID}>=100; ###
-  $seen{$baseID}++; ###
-
+  $seen{$baseID}++;
   $rna{$key}->{$transcript}=$FPKM;
-#  $rnaByTranscript{$transcript}+=$FPKM;
-#  ++$sampleSize{$transcript};
-  #print "$key $transcript $FPKM\n";
+  if($READS) {
+    $rnaByTranscript{$baseID}+=$FPKM;
+    ++$sampleSize{$baseID};
+  }
 }
 close(IN);
 
-open(IN,"$BASE/rna.txt") || die;
-while(<IN>) {
-  chomp; my @fields=split; next unless @fields>=7;
-  my ($indiv,$allele,$gene,$transcript,$cov,$FPKM,$TPM,$nmd)=@fields;
-  next if $indiv eq "indiv"; # header
-  my $key="$indiv $allele";
-  if($transcript=~/(ALT\d+_\S+)_\d+/) { $transcript=$1 }
-  $rnaByTranscript{$transcript}+=$FPKM;
-  ++$sampleSize{$transcript};
+if(!$READS) {
+  open(IN,"$BASE/rna.txt") || die;
+  while(<IN>) {
+    chomp; my @fields=split; next unless @fields>=7;
+    my ($indiv,$allele,$gene,$transcript,$cov,$FPKM,$TPM,$nmd)=@fields;
+    next if $indiv eq "indiv"; # header
+    my $key="$indiv $allele";
+    if($transcript=~/ALT\d+_(\S+)/) { $transcript=$1 }
+    $rnaByTranscript{$transcript}+=$FPKM;
+    ++$sampleSize{$transcript};
+  }
+  close(IN);
 }
-close(IN);
 
 my (%crypticCounts,%exonSkipping,%baseIDs);
 open(IN,$CRYPSKIP) || die $CRYPSKIP;
@@ -92,13 +92,12 @@ for(my $i=0 ; $i<$numKeys ; ++$i) {
     my $mean=$meanFPKM{$baseID};
     next unless $mean>$MIN_FPKM;
     #next if $baseID=~/ENST00000421308.2/; # outlier: common allele
-    #$fpkm/=$mean;
+    next if $seen{$baseID}>$MAX_COPIES;
+    print "dividing $fpkm by $mean\n";
+    $fpkm/=$mean;
     print "$baseID\t$numCryptic\t$fpkm\n";
   }
 }
-
-#indiv   allele  gene    transcript      cov     FPKM    TPM
-#HG00096 1       ENSG00000042088.9       ALT1_ENST00000393452.3  2.948879        0.867289        1.876076
 
 
 
