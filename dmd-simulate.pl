2f8b237cec3a591d1872843fef751206bd19c098
Bill Majoros <martiandna@gmail.com>
2016-09-19 08:35:10 -0400

    update

--- dmd-simulate.pl
+++ dmd-simulate.pl
@@ -1,17 +1,22 @@
 #!/usr/bin/perl
 use strict;
 use GffTranscriptReader;
+use FastaReader;
 
 # Globals and constants
 my $MAX_GROUP_SIZE=10;
+my $SUBSTRATE="ENSG00000198947.10_1";
 my $BASE="/home/bmajoros/1000G/assembly/DMD";
 my $GFF="$BASE/ref-rev.gff";
+my $FASTA="$BASE/ref-rev.fasta";
 my $HEADER="$BASE/header.vcf";
+my $OUT_VCF="$BASE/sim.vcf";
 
-# Load GFF
+# Load GFF & FASTA
 my $reader=new GffTranscriptReader;
 my $transcripts=$reader->loadGFF($GFF);
 my $transcript=$transcripts->[0]; die unless $transcript;
+my $chrom=FastaReader::firstSequence($FASTA);
 
 # Get exon lengths and splice site coordinates
 my (@lengths,@spliceSites);
@@ -51,6 +56,51 @@ for(my $i=1 ; $i+1<$numExons ; ++$i) {
   }
 }
 
+# Simulate splice-disrupting variants
+open(OUT,">$OUT_VCF") || die $OUT_VCF;
+open(IN,$HEADER) || die $HEADER;
+while(<IN>) {
+  if(/^#CHROM/)
+    { print OUT "#CHROM\tPOS\tID\tREF\tALT\tQUAL\tFILTER\tINFO\tFORMAT" }
+  else { print OUT }
+}
+close(IN);
+my @indivs;
+for(my $i=0 ; $i<@spliceSites ; ++$i) {
+  my $sites=$spliceSites[$i];
+  my $exon=$i+2;
+  my $donor=$sites->{donor}; my $acceptor=$sites->{acceptor};
+  my $indiv="\texon${exon}donor"; push @indivs,$indiv; print OUT "\t$indiv";
+  my $indiv="\texon${exon}acceptor"; push @indivs,$indiv; print OUT "\t$indiv";
+}
+print OUT "\n";
+my $numIndivs=@indivs;
+my $indivIndex=0;
+for(my $i=0 ; $i<@spliceSites ; ++$i) {
+  my $sites=$spliceSites[$i];
+  my $exon=$i+2;
+  my $donor=$sites->{donor}; my $acceptor=$sites->{acceptor};
+  my $refDonor=substr($chrom,$donor-1,3);
+  my $refAcceptor=substr($chrom,$acceptor-1,3);
+  simulate($donor,$refDonor,"exon${exon}donor",\*OUT,$indivIndex++,$numIndivs);
+  simulate($acceptor,$refAcceptor,"exon${exon}acceptor",\*OUT,$indivIndex++,
+	  $numIndivs);
+}
+close(OUT);
+
+#======================================================================
+sub simulate {
+  my ($pos,$ref,$indiv,$fh,$indivIndex,$numIndivs)=@_;
+  my $alt=substr($ref,0,1);
+  --$pos;
+  print $fh "$SUBSTRATE\t$pos\t$indiv\t$ref\t$alt\t100\tPASS\tVT=DEL\tGT\t";
+  for(my $i=0 ; $i<$numIndivs ; ++$i) {
+    my $genotype=($i==$indivIndex ? 1 : 0);
+    print $fh "$genotype";
+    if($i+1<$numIndivs) { print $fh "\t" }
+  }
+  print $fh "\n";
+}
 
 #  20      60343   rs527639301     G       A       100     PASS    AC=1;AF=0.000199681;AN=5008;NS=2504;DP=20377;EAS_AF=0;AMR_AF=0.0014;AFR_AF=0;EUR_AF=0;SAS_AF=0;AA=.|||;VT=SNP   GT
 
