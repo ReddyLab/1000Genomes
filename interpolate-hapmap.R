ccc80d43e9834a41dbe227ea5cd3b8d38586a3e6
Bill Majoros <martiandna@gmail.com>
2016-10-05 08:34:09 -0400

    update

--- interpolate-hapmap.R
+++ interpolate-hapmap.R
@@ -5,12 +5,14 @@ chroms <- list("chr1","chr2","chr3","chr4","chr5","chr6","chr7","chr8",
                "chr9","chr10","chr11","chr12","chr13","chr14","chr15",
                "chr16","chr17","chr18","chr19","chr20","chr21","chr22")
 
-for(chr in chroms)
-   hapmapFile <- paste("hapmap/genetic_map_GRCh37_",chr,".txt")
+for(chr in chroms) {
+   hapmapFile <- paste("hapmap/genetic_map_GRCh37_",chr,".txt",sep="")
    hapmap <- read.table(hapmapFile,header=T)
-   thousandFile <- paste("centimorgans/",chr,"chr5.pos")
+   thousandFile <- paste("centimorgans/",chr,".pos",sep="")
    thousand <- read.table(thousandFile,header=F)
    interpolated <- approx(x=hapmap[[2]],y=hapmap[[4]],xout=thousand[[1]])
-   outputFile <- paste(chr,".interp")
-   write.table(interpolated,"chr5.interp")
+   outputFile <- paste(chr,".interpolated",sep="")
+   write.table(interpolated,outputFile)
+}
+
 
