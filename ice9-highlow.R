a5503d6ed72937759736864c0aab91bdb60d7248
Bill Majoros <martiandna@gmail.com>
2016-11-18 06:44:08 -0500

    update

--- ice9-highlow.R
+++ ice9-highlow.R
@@ -1,7 +1,10 @@
 #!/usr/bin/env Rscript
 
-MAXY<-100
+PSEUDOCOUNT=0.0001
+BANDWIDTH=0.002 #0.0002
+MAXY<- 6 #300
 outfile <- "ice9-highlow-noNMD-100.pdf"
+
 pdf(outfile);
 par(mfrow=c(2,1))
 
@@ -14,12 +17,12 @@ data1 <- read.table(infile1);
 data2 <- read.table(infile2);
 
 ###
-#data1 <- data1[data1$V1>0]];
-#data2 <- data2[data2$V1>0]];
+data1 <- log(data1+PSEUDOCOUNT)/log(2)
+data2 <- log(data2+PSEUDOCOUNT)/log(2)
 ###
 
-h1 <- density(data1$V1);
-h2 <- density(data2$V1);
+h1 <- density(data1$V1,bw=BANDWIDTH);
+h2 <- density(data2$V1,bw=BANDWIDTH);
 min1 <- min(data1$V1); max1 <- max(data1$V1);
 min2 <- min(data2$V1); max2 <- max(data2$V1);
 minX <- min(min1,min2); maxX <- max(max1,max2);
@@ -33,7 +36,11 @@ legend("topright",legend=c("P(isoform)<0.1","P(isoform)>0.9"),
        lty=c(1,1),lwd=c(2,2),col=c("blue","red"));
 wilcox.test(data1$V1,data2$V1,alternative="greater")
 
+print("means:")
 mean(data1$V1)
 mean(data2$V1)
+print("medians:")
+median(data1$V1)
+median(data2$V1)
 
 
