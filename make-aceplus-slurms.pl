2059bfc8a52f165aba502bad070661c1cd93fb49
Bill Majoros <martiandna@gmail.com>
2017-01-13 14:14:46 -0500

    update make-aceplus-slurms.pl

--- make-aceplus-slurms.pl
+++ make-aceplus-slurms.pl
@@ -2,13 +2,13 @@
 use strict;
 use SlurmWriter;
 
-my $MAX_INDIV=1;
+my $MAX_INDIV=1000;
 my $JOB_NAME="ACE+";
 my $GEUVADIS_ONLY=1;
-my $PROGRAM="ace.pl";
-my $OUT1="ace1-new.essex"; # "1.essex"
-my $OUT2="ace2-new.essex"; # "2.essex"
-my $MODEL="/home/bmajoros/1000G/ACE/model";
+my $PROGRAM="aceplus.pl";
+my $OUT1="aceplus.essex"; # "1.essex"
+my $OUT2="aceplus.essex"; # "2.essex"
+my $MODEL="/home/bmajoros/1000G/ACEPLUS/model";
 my $MAX_ERRORS=0;
 my $THOUSAND="/home/bmajoros/1000G";
 my $ASSEMBLY="$THOUSAND/assembly";
@@ -24,13 +24,13 @@ foreach my $subdir (@dirs) {
   next unless $subdir=~/^HG\d+$/ || $subdir=~/^NA\d+$/;
   next unless -e "$COMBINED/$subdir/RNA/stringtie.gff";
   my $dir="$COMBINED/$subdir";
-  $writer->addCommand("cd $dir ; /home/bmajoros/ACEPLUS/$PROGRAM  $MODEL  $REF_FASTA  $dir/1.fasta  /home/bmajoros/1000G/assembly/local-genes.gff  $MAX_ERRORS  $dir/$OUT1");
-  $writer->addCommand("cd $dir ; /home/bmajoros/ACEPLUS/$PROGRAM  $MODEL  $REF_FASTA  $dir/2.fasta  /home/bmajoros/1000G/assembly/local-genes.gff  $MAX_ERRORS  $dir/$OUT2");
+  $writer->addCommand("cd $dir ; /home/bmajoros/ACEPLUS/$PROGRAM  $MODEL  $REF_FASTA  $dir/1.ice9.fasta  /home/bmajoros/1000G/assembly/local-genes.gff  $MAX_ERRORS  $dir/$OUT1");
+  $writer->addCommand("cd $dir ; /home/bmajoros/ACEPLUS/$PROGRAM  $MODEL  $REF_FASTA  $dir/2.ice9.fasta  /home/bmajoros/1000G/assembly/local-genes.gff  $MAX_ERRORS  $dir/$OUT2");
   ++$numIndiv;
   if($numIndiv>=$MAX_INDIV) { last }
 }
 $writer->mem(5000);
 $writer->setQueue("new,all");
-$writer->writeArrayScript($SLURM_DIR,$JOB_NAME,$SLURM_DIR,500);
+$writer->writeArrayScript($SLURM_DIR,$JOB_NAME,$SLURM_DIR,1000);
 
 
